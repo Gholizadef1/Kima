c98108595fda7d80427499b3097e22bab099dbdd
f_gholizade13 <gholizadefatemeh37@gmail.com>
2020-10-29 12:58:46 -0400

    Styling_HomePage

---
+++ Webfront/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Switch from 'react-bootstrap/esm/Switch';
+//import { BrowserRouter, Route } from 'react-router-dom';
+import *as ReactBootstrap from "react-bootstrap";
+//import BrowserRouter from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+//import { IconName } from "react-icons/gi";
+
+import Slide from './slides/Slide';
+import {NavBar} from "./Components/Navbar";
+
+//import Header from "./images/image";
+//import {pricing} from "./Components/Pricing";
+//import {Navigation} from "./Components/Navigation";
+class App extends Component {
+  render(){
+  return (
+      <BrowserRouter>
+      <div className = "App">
+     <NavBar/>
+  
+ 
+  <Slide/>
+
+
+<Switch>
+  <Route path = '/' component = {NavBar} exact/>
+ 
+</Switch>
+        
+      </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ Webfront/Components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const dank = () => {
+    return(
+        <div className="App">
+            <h1>Profile</h1>
+        </div>
+    )
+}
+
+export default dank;
