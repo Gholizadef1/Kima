614c01df265a921bc147e687b885b6b7361ffcf9
MelikaNobakhtian <m_nobakhtian@comp.iust.ac.ir>
2020-11-11 10:10:10 -0800

    fix some details

--- BackEnd/tutorial/tutorial/settings.py
+++ BackEnd/tutorial/tutorial/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'tutorial.kyma',
     'tutorial.quickstart',
     'rest_framework.authtoken'
@@ -54,6 +55,8 @@ INSTALLED_APPS = [
 # }
 
 MIDDLEWARE = [
+    #'corsheaders.middleware.CorsMiddleware',
+    #'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -154,3 +157,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',]
 }
 AUTH_USER_MODEL = 'quickstart.Account'
+
+CORS_ORIGIN_ALLOW_ALL = True
