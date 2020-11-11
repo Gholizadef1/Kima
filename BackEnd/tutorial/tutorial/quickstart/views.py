b1788fc39dc6a4fce2022e367bd82999196cd515
setayeshk <setayeshk12345@gmail.com>
2020-11-11 18:27:06 +0330

    fix login errors

--- BackEnd/tutorial/tutorial/quickstart/views.py
+++ BackEnd/tutorial/tutorial/quickstart/views.py
@@ -71,12 +71,12 @@ class SignUp_view(APIView):
 @permission_classes([AllowAny],)
 @permission_classes([IsAuthenticated])
 def login(request):
-    username = request.data.get("username")
+    email = request.data.get("email")
     password = request.data.get("password")
-    if username is None or password is None:
+    if email is None or password is None:
         return Response({'error': 'Please provide both username and password'},
                         status=HTTP_400_BAD_REQUEST)
-    user = authenticate(username=username, password=password)
+    user = authenticate(email=email, password=password)
     if not user:
         return Response({'error': 'Invalid Credentials'},
                         status=HTTP_404_NOT_FOUND)
--- BackEnd/tutorial/tutorial/settings.py
+++ BackEnd/tutorial/tutorial/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['312cfbacb4a5.ngrok.io']
 
 
 # Application definition
