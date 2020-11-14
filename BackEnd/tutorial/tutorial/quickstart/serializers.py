d5a8c62f678472a2016912b85408321346c649d2
f_gholizade13 <gholizadefatemeh37@gmail.com>
2020-11-14 13:21:32 -0500

    Nothing

---
+++ BackEnd/tutorial/tutorial/quickstart/serializers.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from .models import Account
+#from .models import SignUpUser
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type': 'password'},write_only=True)
+
+    class Meta:
+        model = Account
+        fields = ['email', 'username','password','password2']
+        extra_kwargs = {
+                'password':{'write_only': True}
+        }
+
+    def save(self):
+        account=Account(
+            email=self.validated_data['email'],
+            username=self.validated_data['username'],
+        )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match.'})
+        account.set_password(password)
+        account.save()
+        return account
+
+
+
--- webfront/src/Components/UsersList.js
+++ webfront/src/Components/UsersList.js
@@ -1,32 +1,45 @@
-import React, { useState } from 'react';
-import './users.css';
-import Sresult from './Sresult';
-import {Link} from 'react-router-dom';
-import HelpingNavbar, {NavBar} from './HelpingNavbar';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import { Button, Navbar } from 'react-bootstrap';
-const UsersList = () =>{
-  const [img,setImg]= useState("");
-  const inputEvent=(event)=>{
-    const data = event.target.value;
-    console.log(data);
-    setImg(data);
+import React, { Component } from "react";
+import axios from "axios";
+class UsersList extends Component {
+  state = {
+    title: "",
+    body: ""
   };
-  
-  return(
-  
-    <div className="searchbar">
-    <input type ="text" 
-    placeholder="...جستجو" 
-    value={img}
-    onChange={inputEvent}/>
-   {img === ""? null: <Sresult name={img}/>}
-    </div>
-    
-  );
-};
+
+  onTitleChange = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+
+  onBodyChange = e => {
+    this.setState({
+      body: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const data = {
+      title: this.state.title,
+      body: this.state.body
+    };
+    axios.post("https://jsonplaceholder.typicode.com/posts", data)      .then(res => console.log(res))      .catch(err => console.log(err));  };
+
+  render() {
+    return (
+      <div className="post">
+        <form className="post" onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Title" value={this.state.title}
+            onChange={this.onTitleChange} required
+          />
+          
+          
+          <button type="submit">Create Post</button>
+        </form>
+      </div>
+    );
+  }
+}
+
 export default UsersList;
