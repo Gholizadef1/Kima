f11a856e861ccec1bef31b11faf7d47cdd0fe002
f_gholizade13 <gholizadefatemeh37@gmail.com>
2020-11-15 13:08:00 -0500

    Last two Gets

--- webfront/src/Components/UsersList.js
+++ webfront/src/Components/UsersList.js
@@ -1,45 +1,70 @@
-import React, { Component } from "react";
-import axios from "axios";
-class UsersList extends Component {
-  state = {
-    title: "",
-    body: ""
-  };
-
-  onTitleChange = e => {
-    this.setState({
-      title: e.target.value
-    });
-  };
-
-  onBodyChange = e => {
-    this.setState({
-      body: e.target.value
-    });
-  };
-  handleSubmit = e => {
-    e.preventDefault();
-    const data = {
-      title: this.state.title,
-      body: this.state.body
-    };
-    axios.post("https://jsonplaceholder.typicode.com/posts", data)      .then(res => console.log(res))      .catch(err => console.log(err));  };
-
-  render() {
-    return (
-      <div className="post">
-        <form className="post" onSubmit={this.handleSubmit}>
-          <input
-            placeholder="Title" value={this.state.title}
-            onChange={this.onTitleChange} required
-          />
-          
-          
-          <button type="submit">Create Post</button>
-        </form>
-      </div>
-    );
+import axios from 'axios';
+import React, { useState, useEffect } from "react";
+function UsersList() {
+  const [user,setUser] = useState({user:""});
+  const [search,setSearch] = useState([]);
+  const [users,setUsers] = useState([]);
+ 
+ const handleChange = event => {
+    setUser({ user: event.target.value });
   }
+
+const searchUsers = async () => {
+try {
+const result = await axios.get(`http://127.0.0.1:8000/dyanmicsearch`,{params:{
+  search:user.user,
+  search_fields:'author',}
+});
+if (search == [null]) {
+setSearch([]);
+} else {
+setSearch(result.data);
 }
+}
+catch (err) {}
+};
+useEffect(()=>{
+  const UsersList = () => {
+    axios.get(`http://127.0.0.1:8000/dyanmicsearch/`)
+    .then((res)=> {
+      setUser([res.data])
+    })
+    .catch((error)=>{});
+  };
+    UsersList();
+
+},[]);
+useEffect(() => {
+  searchUsers();
+  }, [user]);
+
+    return ( 
+    <div className = "search">
+      {search.map((item) => (
+        <div className="out" key={item.id}>
+        <h5 className="card-title2">{item.name}</h5>
+         <div className="card cat">
+            <img
+              className="squere" 
+              src={item.imgurl}
+              height={56}
+              width={56}
+            />
+    
+        <label>
+            <input type="text" name="name" onChange={handleChange}  value={user.user} />
+          </label>
+          <button type="submit">Search</button>
+          </div>
+          </div>
+          
+        )
+        )            
+}
+        
+ </div>
+    );
+}  
+ 
 
 export default UsersList;
