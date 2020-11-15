ed51ae7c3f7398ee232be0c72ad582077d983970
CE9752 <fateme.ahmadi1522@gmail.com>
2020-11-15 16:37:23 +0330

    *.pyc deleted

--- BackEnd/tutorial/tutorial/quickstart/views.py
+++ BackEnd/tutorial/tutorial/quickstart/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
