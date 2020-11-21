00665d7113c8f05b1a7cb1175c23561a0f4175c5
CE9752 <fateme.ahmadi1522@gmail.com>
2020-11-21 18:57:39 +0330

    fix setting

--- BackEnd/tutorial/tutorial/settings.py
+++ BackEnd/tutorial/tutorial/settings.py
@@ -58,8 +58,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
