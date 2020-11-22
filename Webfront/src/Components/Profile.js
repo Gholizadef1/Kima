85e2dc00be014e5b9d4bba6a25262c2c1f92b4e1
fomid8 <fati.omid2000@gmail.com>
2020-11-22 22:12:57 +0330

    profile init

--- Webfront/src/Components/Profile.js
+++ Webfront/src/Components/Profile.js
@@ -1,24 +1,52 @@
-import React, {Component} from "react";
-import {Redirect} from "react-router-dom";
-import { Button } from 'react-bootstrap';
+import React, {Component,useState,useEffect} from "react";
+import {Redirect,withRouter} from "react-router-dom";
+import axios from 'axios';
+//import { Button } from 'react-bootstrap';
 import "./Profile.css";
 
-class Profile extends Component{
-    state={
-        navigate:false
-    };
-    logout = () =>{
-        localStorage.clear("token");
-        this.setState({navigate:true});
-    };
-    render(){
-        const{navigate} = this.state;
-        if(navigate){
-            return <Redirect to = "/register" push = {true}/>;
-        }
-        return  <button type="button" class="btn-default"onClick = {this.logout} >خروج ازحساب</button>
-    } 
+// class Profile extends Component{
+//     state={
+//         navigate:false
+//     };
+//     logout = () =>{
+//         localStorage.clear("token");
+//         this.setState({navigate:true});
+//     };
+//     render(){
+//         const{navigate} = this.state;
+//         if(navigate){
+//             return <Redirect to = "/register" push = {true}/>;
+//         }
+//         return  <button type="button" class="btn-default"onClick = {this.logout} >خروج ازحساب</button>
+//     } 
    
+// }
+
+// export default Profile;
+
+function ProFile (props){
+    const [state , setState]=useState(
+        {
+            navigate:false
+
+        }
+    )
+    const logout = () =>{
+        localStorage.clear("token");
+        props.history.push('/login');
+    }
+
+    return(
+        
+        <div>
+            
+            <button type="button" class="btn-default" onClick = {logout} >
+                خروج ازحساب
+            </button>
+        </div>
+    )
 }
 
-export default Profile;
+export default withRouter(ProFile);
+
+
