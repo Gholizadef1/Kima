3d82eca2435a1cf7a1a4005b4fe06978ca61b77a
f_gholizade13 <gholizadefatemeh37@gmail.com>
2020-11-25 14:30:08 -0500

    Post Completed

--- Webfront/src/Components/Avatar.js
+++ Webfront/src/Components/Avatar.js
@@ -1,35 +1,72 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import {CgProfile} from 'react-icons/cg';
 import "./Avatar.css";
 import axios from "axios";
-import { Form,Button } from "react-bootstrap";
-import { event } from "jquery";
-class Avatar extends React.Component {
- state={
-   selectedfile:this.state.selectedfile
- }
-  fileSelectedHandler=event=>{
-    this.setState({
-    selectedfiles:event.target.files[0]
-    })
+function Avatar() {
+  const state = {
+    file:null
   }
-  uploadHandler=()=>{
-    const fd = new FormData();
-    fd.append('image',this.state.selectedfile,this.state.selectedfile.name);
+     const uploadedImage = React.useRef(null);
+    const imageUploader = React.useRef(null);
+    const handleImageUpload = e => {
+      const [file] = e.target.files;
+      if (file) {
+        const reader = new FileReader();
+        this.setState({file:false})
+        const { current } = uploadedImage;
+        current.file = file;
+        reader.onload = e => {
+          current.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    };
+    const handleUpload= e =>{
+  let file=this.state.file
+  let formdata = new FormData()
+  formdata.append("image",file)
+  
+  axios({
+    url:'api/update-profile',
+    method:"POST",
+    headers:{
+    
+    }
+  })
+}
+  return (
+   
+    <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
 
-axios.put('api/update-profile',fd)
-.then(res=>{
-  console.log(res);
-});
-}
-  render(){
-    return (
-      <div>
-     <input type="file" onChange={this.fileSelectedHandler}/>
-     <button onClick={this.uploadHandler}>upload</button>
-     </div>
-    );
-  }
-}
+    }}
+  >
+    <input
+      type="file"
+      accept="image/*"
+      onChange={handleImageUpload}
+      ref={imageUploader}
+      style={{
+        display: "none"
+      }}
+    />
+    <div
+      style={{
+        height: "60px",
+        width: "60px",
+        border: "1px dashed black"
+      }}
+      onClick={() => imageUploader.current.click()}
+    >
+      <img className="avatar"
+
+        ref={uploadedImage}
+      />
+      <button type="button"onClick={handleUpload}>Upload</button>
+    </div>
+
+  </div>
+  );
+        }
 export default Avatar;  
