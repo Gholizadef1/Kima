75258d96de1331a167ea9559cff91cfd33044134
setayeshk <setayeshk12345@gmail.com>
2020-11-26 14:53:48 +0330

    Add Textinputs 1

--- Appfront/src/Screens/Editprofile.js
+++ Appfront/src/Screens/Editprofile.js
@@ -1,6 +1,6 @@
-import React,{useContext} from 'react';
+import React,{useContext,useState} from 'react';
 import { StyleSheet, Text, View,Image,ImageBackground,Alert ,ScrollView} from 'react-native';
-import {Container,Header,Title,Button,Form,Item,Input, Icon} from 'native-base';
+import {Container,Header,Title,Form,Item,Input,Button, Icon,CheckBox,Body, ActionSheet} from 'native-base';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { createStackNavigator } from 'react-navigation-stack';
 import { Avatar } from 'react-native-paper';
@@ -20,11 +20,53 @@ import { Feather } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import Animated from 'react-native-reanimated';
 import BottomSheet from 'reanimated-bottom-sheet';
+import {Formik,formik} from 'formik';
+import * as yup from 'yup';
 
+const signschema=yup.object({
 
+    Username:yup.string()
+    .required()
+    .min(4),
+  
+    Email:yup.string()
+    .required()
+    .min(8)
+    .email('invalid email format'),
+  
+    Password:yup.string()
+    .required()
+    .min(5),
+  
+    repeatPassword:yup.string()
+    .required()
+    .min(5)
+    .oneOf([yup.ref('Password'),''],'Password must match')
+    // .test('equaltopass','this should be the same as your password',(val)=>{
+    //   return repeatPassword.string()===repeatPassword.string();
+    // })
+  
+  
+  
+  
+  })
+const userschema=yup.object({
 
+    Username:yup.string()
+    .required()
+    .min(4),
+  
+  })
+  const passschema=yup.object({
+  
+    Password:yup.string()
+    .required()
+    .min(5),
+  
+  })
 const Profile = ({navigation}) => {
-
+   
+  
     const val = useContext(AuthContext);  
     //nd
     const fall=new Animated.Value(1);
@@ -98,7 +140,7 @@ const Profile = ({navigation}) => {
           {/* <ScrollView style={{height:1000}}> */}
         <Header style={{marginTop:35,backgroundColor:'white',position:'absolute'}}></Header>
         <Text style={styles.kima}>کیما</Text>
-        <Text>Editprofile</Text>
+       
         {/* <Avatar.Image style={styles.avatar} size={80}
         source={require('../../assets/avatar.png')}
         ></Avatar.Image> */}
@@ -111,55 +153,274 @@ const Profile = ({navigation}) => {
             ref={bs}
             initialSnap={1}
             callbackNode={fall}
-            // enabledGestureInteraction={true}
+            enabledGestureInteraction={true}
             renderContent={renderinner}
             renderHeader={renderheader}
-            
+            onCloseEnd={bs}
+
+                        
                // style={{position:'absolute',height:200,width:250,marginTop:400}}
             backgroundColor={'white'}
         
         ></BottomSheet>
         
-        <View style={{position:'absolute',height:100,width:100,marginTop:200,marginLeft:50,borderRadius:15}}>
+        <View style={{position:'absolute',height:100,width:100,marginTop:150,marginLeft:150,borderRadius:15}}>
         <TouchableOpacity
         onPress={()=>{bs.current.snapTo(0)}}
         >
         <ImageBackground borderRadius={15}
         source={require('../../assets/avatar.png')}
-        style={{height:100,width:100,borderRadius:15}}
+        style={{height:100,width:100,borderRadius:1}}
         
         >
 
         </ImageBackground>
         </TouchableOpacity>
         </View>
+
+        <Formik style={{borderStyle:'dashed',justifyContent:'space-around'}}
+      initialValues={{Email:'',Username:'',Password:'',repeatPassword:'' }}
+      validationSchema={signschema}
+      
+
+        onSubmit={async(values,actions)=>{
+        //  signup(values);
+        const back={
+          username:values.Username,
+          email:values.Email,
+          password:values.Password,
+          password2:values.repeatPassword,
+        
+        }
+         const backk=JSON.stringify(back);
+        const params=JSON.stringify({username:'Hi',email:'Hi@Hi.Hi',password:'12345',password2:'12345'});
+        axios.post('http://1d5bf2d8221a.ngrok.io/register',backk,{"headers":{"content-type":"application/json",}})
+        .then(async function(response){
+          AsyncStorage.setItem('token',response.data.token)
+          
+            if(response.data.email!==back.email||response.data.username!==back.username){
+            if(response.data.email!==undefined&&response.data.username!==undefined){
+              console.log(response.data.username);
+              console.log(response.data.email);
+              Alert.alert('oops','کاربری با این ایمیل و نام کاربری از قبل وجود دارد',[{
+
+            title:'فهمیدم',onPress:()=>console.log('alert closed')
+              }])
+            }
+            else if(response.data.username!==undefined){
+            Alert.alert('oops','کاربری با این نام کاربری از قبل وجود دارد',[{
+              title:'فهمیدم',onPress:()=>console.log('alert closed')
+            }])
+            
+          }
+          else if(response.data.email!==undefined){
+            Alert.alert('oops','کاربری با این ایمیل از قبل وجود دارد',[{
+              title:'فهمیدم',onPress:()=>console.log('alert closed')
+            }])
+          }
+            else{
+              Alert.alert('oops','لطفا ایمیل درستی را وارد کنید',[{
+
+            title:'فهمیدم',onPress:()=>console.log('alert closed')
+              }])
+              
+            }
+            }
+             else{
+            //  AsyncStorage.setItem('token',response.data.token)
+             console.log('inja')
+            console.log(back.email);
+            pro.navigation.navigate('Log');
+            actions.resetForm();
+          }
+  
+          //  console.log(response);
+        })
+        .catch(function(error){
+            console.log(error);
+         
+        })
+
+      }}
+     >
+     {(props)=>(
+     <View style={{alignItems:'center', marginTop:280,marginHorizontal:40}}>
+
+     <Item style={styles.input}>
+
+         <Input style={styles.Input} autoCapitalize='words' autoCorrect={true}
+         onChangeText={props.handleChange('Username')}
+         onBlur={props.handleBlur('Username')}
+         value={props.values.Username}
+         placeholder="نام  ..." placeholderTextColor='lightgray'>
+         </Input>
+         <AntDesign name="user" size={24} color="#BFDBF7" style={styles.Icon} />
+        
+       </Item>
+
+      
+       <View style={{flexDirection:'row',width:400,marginRight:10,marginLeft:10}}>
+       
+    
+          
+   
+     <Button bordered rounded style={styles.button}
+       onPress={props.handleSubmit}
+       >
+         <Text style={{color:'#1f7a8c', fontSize:15,fontWeight:'bold', marginHorizontal:60,marginLeft:70}}>تایید</Text>
+        </Button>
+      
+     </View>
+    
+     </View>
+       
+       
+     )}
+
+     </Formik>
+     <Formik style={{borderStyle:'dashed',justifyContent:'space-around'}}
+      initialValues={{Email:'',Username:'',Password:'',repeatPassword:'' }}
+      validationSchema={signschema}
+      
+
+        onSubmit={async(values,actions)=>{
+        //  signup(values);
+        const back={
+          username:values.Username,
+          email:values.Email,
+          password:values.Password,
+          password2:values.repeatPassword,
+        
+        }
+         const backk=JSON.stringify(back);
+        const params=JSON.stringify({username:'Hi',email:'Hi@Hi.Hi',password:'12345',password2:'12345'});
+        axios.post('http://1d5bf2d8221a.ngrok.io/register',backk,{"headers":{"content-type":"application/json",}})
+        .then(async function(response){
+          AsyncStorage.setItem('token',response.data.token)
+          
+            if(response.data.email!==back.email||response.data.username!==back.username){
+            if(response.data.email!==undefined&&response.data.username!==undefined){
+              console.log(response.data.username);
+              console.log(response.data.email);
+              Alert.alert('oops','کاربری با این ایمیل و نام کاربری از قبل وجود دارد',[{
+
+            title:'فهمیدم',onPress:()=>console.log('alert closed')
+              }])
+            }
+            else if(response.data.username!==undefined){
+            Alert.alert('oops','کاربری با این نام کاربری از قبل وجود دارد',[{
+              title:'فهمیدم',onPress:()=>console.log('alert closed')
+            }])
+            
+          }
+          else if(response.data.email!==undefined){
+            Alert.alert('oops','کاربری با این ایمیل از قبل وجود دارد',[{
+              title:'فهمیدم',onPress:()=>console.log('alert closed')
+            }])
+          }
+            else{
+              Alert.alert('oops','لطفا ایمیل درستی را وارد کنید',[{
+
+            title:'فهمیدم',onPress:()=>console.log('alert closed')
+              }])
+              
+            }
+            }
+             else{
+            //  AsyncStorage.setItem('token',response.data.token)
+             console.log('inja')
+            console.log(back.email);
+            pro.navigation.navigate('Log');
+            actions.resetForm();
+          }
+  
+          //  console.log(response);
+        })
+        .catch(function(error){
+            console.log(error);
+         
+        })
+
+      }}
+     >
+     {(props)=>(
+     <View style={{alignItems:'center', marginTop:100,marginHorizontal:40}}>
+
+     <Item style={styles.input}>
+
+         <Input style={styles.Input} autoCapitalize='words' autoCorrect={true}
+         onChangeText={props.handleChange('Username')}
+         onBlur={props.handleBlur('Username')}
+         value={props.values.Username}
+         placeholder="رمز خود را وارد کنید  ..." placeholderTextColor='lightgray'>
+         </Input>
+         <AntDesign name="user" size={24} color="#BFDBF7" style={styles.Icon} />
+        
+       </Item>
+       <Item style={styles.input}>
+         <Input  style={styles.Input} 
+          secureTextEntry
+         onChangeText={props.handleChange('Password')}
+         value={props.values.Password}
+         onBlur={props.handleBlur('Password')}
+         placeholder="رمز جدید خود را وارد کنید ..." placeholderTextColor='lightgray'>
+         </Input>
+         <AntDesign name="lock" size={24} color="#BFDBF7"  style={styles.Icon}/>
+       </Item>
+
+    
+      
+       <Text style={{fontSize:10, color:'red'}}>{props.touched.Password&&props.errors.Password}</Text>
+
+       
+       <Item style={styles.input}>
+         <Input  style={styles.Input} 
+          secureTextEntry
+          onChangeText={props.handleChange('repeatPassword')}
+          value={props.values.repeatPassword}
+          onBlur={props.handleBlur('repeatPassword')}
+          placeholder="رمز جدید خود را تکرار کنید" placeholderTextColor='lightgray'>
+         </Input>
+         <Feather name="check" size={24} color="#BFDBF7" style={styles.Icon} />
+       </Item>
+      
+       <Text style={{fontSize:10, color:'red'}}>{props.touched.repeatPassword&&props.errors.repeatPassword}</Text>
+       
+
+
+
+  
+
+      
+       <View style={{flexDirection:'row',width:400,marginRight:10,marginLeft:10}}>
+       
+       
+       
+    
+          
+   
+     <Button bordered rounded style={styles.button}
+       onPress={props.handleSubmit}
+       >
+         <Text style={{color:'#1f7a8c', fontSize:15,fontWeight:'bold', marginHorizontal:60,marginLeft:70}}>تایید</Text>
+        </Button>
+      
+     </View>
+    
+     </View>
+       
+       
+     )}
+
+     </Formik>
+        
         {/* </View>
         </TouchableOpacity>
   */}
   {/* //<View style={{position:'absolute',marginTop:700,width:380,height:100}}> */}
    
         {/* </TouchableOpacity> */}
-        <Text style={{marginTop:200,marginRight:42,color:"#1F7A8C"}}>نام کاربری <Text style={styles.donoghte}>:  </Text><Text style={{color:'black',width:100}}>سلام</Text></Text>
-        <AntDesign name="user" size={24} color="#BFDBF7"  style={styles.Icon}/>
-    
-        <Text style={styles.info}>ایمیل <Text style={styles.donoghte}>:</Text><Text style={{color:'black',width:100}}>hi@hi.hi</Text></Text>
-        <Feather name="mail" size={20} color="#BFDBF7" style={{ position:'absolute', height:20, marginTop:367, marginLeft:378}} />
-
-        <Button style={styles.edit}>
-            <Text style={{marginLeft:55,color:'#1F7A8C'}}>ویرایش پروفایل</Text>
-        </Button>
-        <Image
-         source={require('../../assets/line3.png')}
-         style={{marginTop:492,marginHorizontal:10,width:50,height:1,position:'absolute',marginLeft:45}}
-         ></Image>
-          <Image
-         source={require('../../assets/line3.png')}
-         style={{marginTop:492,marginHorizontal:10,width:50,height:1,position:'absolute',marginLeft:310}}
-         ></Image>
-
-         
-
-    
+      
     {/* </ScrollView> */}
      
     <StatusBar backgroundColor='#BFDBF7' style='light' />
@@ -214,7 +475,7 @@ const styles = StyleSheet.create({
     
       
         marginTop:300,
-        marginLeft:378
+        marginLeft:300
     },
     avatar:{
         position:'absolute',
@@ -229,6 +490,24 @@ const styles = StyleSheet.create({
         borderRadius:25,
         marginLeft:105,
         marginTop:80
-    }
+    },
+    button:{
+        position:'absolute',
+        marginTop:50,
+        marginLeft:10,
+        width:170,
+        backgroundColor:'#E1E5F2',
+        borderColor:'#BFDBF7',
+        marginLeft:112,
+        borderRadius:17
+        
+      },
+      Input:{
+        fontSize:15,
+        fontStyle:'normal',
+        marginRight:40
+        
+        
+      },
   });
   export default Profile;
