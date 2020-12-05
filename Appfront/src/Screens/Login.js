e45e9645fb3233b7bac022e1c26d3489b976cea0
setayeshk <setayeshk12345@gmail.com>
2020-12-05 19:29:02 +0330

    Fix login post request url

--- Appfront/src/Screens/Login.js
+++ Appfront/src/Screens/Login.js
@@ -63,7 +63,7 @@ const logschema=yup.object({
         }
          const backk=JSON.stringify(back);
 
-         axios.post('http://eb506fafbc32.ngrok.io/login/',backk,{"headers":{"content-type":"application/json",}})
+         axios.post('http://eb506fafbc32.ngrok.io/login',backk,{"headers":{"content-type":"application/json",}})
         .then(async function(response){
           console.log(response.data.userid)
            console.log(response)
