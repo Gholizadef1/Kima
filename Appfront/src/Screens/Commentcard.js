a40f13f64f635c60e4fdbdc373bb87b9611a527c
setayeshk <setayeshk12345@gmail.com>
2020-12-07 12:52:34 +0330

    Add avatar logo to Commentcard

--- Appfront/src/Screens/Commentcard.js
+++ Appfront/src/Screens/Commentcard.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Left, Body, Right, Title, CardItem, Card } from 'native-base';
+import { Avatar } from 'react-native-paper';
 
 const Commentcard = () => {
     return(
         <View style={styles.container}>
-            <Text>
-                Groups
+            {/* <Text>
+                Comment
             </Text>
+            <Card><Text>hi</Text></Card> */}
+            
+            <Avatar.Image style={styles.avatar}></Avatar.Image>
+            <Text>name</Text>
+        
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
+      marginTop:40,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+    
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
     },
+    avatar:{
+        marginRight:'50%',
+        marginTop:'5%'
+    
+    }
   });
   export default Commentcard;
