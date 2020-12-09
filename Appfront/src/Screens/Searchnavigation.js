508b952857c643af89c74a0560b2153c4dfe8d7e
setayeshk <setayeshk12345@gmail.com>
2020-12-09 16:22:35 +0330

    Add dynamic header for SearchNavigation

--- Appfront/src/Screens/Searchnavigation.js
+++ Appfront/src/Screens/Searchnavigation.js
@@ -26,7 +26,7 @@ const Searchnavigation=()=>{
     <SNavigation.Navigator initialRouteName={'Search'}>
       <SNavigation.Screen name='Search' component={Search} options={{headerShown: false}} ></SNavigation.Screen>
       <SNavigation.Screen name="Showbookview"   component={Bookview} options={{headerShown: false}}></SNavigation.Screen>
-      <SNavigation.Screen name="comment"   component={Comment} options={{headerShown: false}}></SNavigation.Screen>
+      <SNavigation.Screen name="comment"   component={Comment} options={({route}) => ({title: route.params.title})}></SNavigation.Screen>
 
     </SNavigation.Navigator>
   
