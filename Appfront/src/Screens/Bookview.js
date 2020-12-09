8fd12ec4509ed366b650a8b0996f0b0f617886c8
setayeshk <setayeshk12345@gmail.com>
2020-12-09 21:03:38 +0330

    Add button to Bookview for navigating to Quote page

--- Appfront/src/Screens/Bookview.js
+++ Appfront/src/Screens/Bookview.js
@@ -3,7 +3,7 @@ import { StyleSheet, View , Image , ImageBackground , ScrollView ,
 TouchableOpacity , FlatList , TextInput } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Text, Button, Icon, Body,
    Right, Left , Picker, Form } from 'native-base';
-
+import Quote from './Quote';
 import Home from './Home';
 import PickerShow from './PickerShow'
 import axiosinst from '../api/axiosinst'
@@ -56,6 +56,9 @@ const Bookview = (prop) => {
             <Button onPress={()=>{prop.navigation.navigate('comment',{title:result.title,imgurl:result.imgurl})&& prop.navigation.setOptions({
       title: response.data.title,
     });}}><Text>صفحه نظرات</Text></Button>
+           <Button onPress={()=>{prop.navigation.navigate('quote',{title:result.title,imgurl:result.imgurl})&& prop.navigation.setOptions({
+      title: response.data.title,
+    });}}><Text>صفحه نقل قول ها</Text></Button>
             <StatusBar backgroundColor='#BFDBF7' style='light' />
       </Container>
     );
--- Appfront/src/Screens/Quote.js
+++ Appfront/src/Screens/Quote.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const Quote = () => {
+const Quote = (prop) => {
     return(
         <View style={styles.container}>
             <Text>
