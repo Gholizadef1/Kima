e688fbe5044767d9a963c3ece76ea7e5531396ea
setayeshk <setayeshk12345@gmail.com>
2020-12-09 20:34:36 +0330

    Add Quote navigation to Searchnavigation

--- Appfront/src/Screens/Searchnavigation.js
+++ Appfront/src/Screens/Searchnavigation.js
@@ -14,10 +14,12 @@ import Search from './Search';
 import Bookview from './Bookview';
 import Commentcard from './Commentcard';
 import Comment from './Comment';
+import Quote from './Quote';
 
 // import TabScreen from './TabScreen';
  import Home from './Home';
  import {NavigationContainer} from '@react-navigation/native';
+
  
  const SNavigation = createStackNavigator();
 const Searchnavigation=()=>{
@@ -27,6 +29,11 @@ const Searchnavigation=()=>{
       <SNavigation.Screen name='Search' component={Search} options={{headerShown: false}} ></SNavigation.Screen>
       <SNavigation.Screen name="Showbookview"   component={Bookview} options={{headerShown: false}}></SNavigation.Screen>
       <SNavigation.Screen name="comment"   component={Comment}  options={({route}) => ({title: route.params.title,headerTintColor:'#1f7a8c'
+      ,headerTitleStyle:{fontSize:18,fontWeight:'bold'
+      },headerStyle:{backgroundColor:'#EDF2F4'}
+      })}></SNavigation.Screen>
+
+<SNavigation.Screen name="quote"   component={Quote}  options={({route}) => ({title: route.params.title,headerTintColor:'#1f7a8c'
       ,headerTitleStyle:{fontSize:18,fontWeight:'bold'
       },headerStyle:{backgroundColor:'#EDF2F4'}
       })}></SNavigation.Screen>
