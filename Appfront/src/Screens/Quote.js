448b4773de60fdc9aec37d468812d72b0b8257b5
setayeshk <setayeshk12345@gmail.com>
2020-12-10 18:49:20 +0330

    Add scrollview to Quote component

--- Appfront/src/Screens/Quote.js
+++ Appfront/src/Screens/Quote.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import Quotecrad from './Quotecard';
 
 const Quote = (prop) => {
     return(
         <View style={styles.container}>
+        <ScrollView>
            <Quotecrad></Quotecrad>
+           <Quotecrad></Quotecrad>
+           </ScrollView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+    
+        flex: 1,
+        // backgroundColor: '#B8B8B8',
+        backgroundColor:'#ffff',
+        marginTop:1
     },
   });
   export default Quote;
