59ee04ead6fbe8856d3f68418a4df67e56bfc0d1
setayeshk <setayeshk12345@gmail.com>
2020-12-10 17:05:38 +0330

    Add quote navigation to Booknavigation

--- Appfront/src/Screens/Booknavigation.js
+++ Appfront/src/Screens/Booknavigation.js
@@ -6,14 +6,23 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from './Home';
 import Bookview from './Bookview';
 
+import Quote from './Quote';
+
 const Stack = createStackNavigator();
 
 const Booknavigation = () => {
   return (
+    // options={({navigation})=>({headerTitle:navigation.getParam()})}
       <Stack.Navigator initialRouteName={'Home'}>
       <Stack.Screen name = 'Home' component={Home} options={{headerShown: false}} ></Stack.Screen>
       <Stack.Screen name = 'Showbookview' component={Bookview} options={{headerShown: false}} />
+      <Stack.Screen name = 'quote' component={Quote}  options={({route}) => ({title: route.params.title,headerTintColor:'#1f7a8c'
+      ,headerTitleStyle:{fontSize:18,fontWeight:'bold'
+      },headerStyle:{backgroundColor:'#EDF2F4'}
+      })}/>
       </Stack.Navigator>
+      
+
   );
 }
 export default Booknavigation;
