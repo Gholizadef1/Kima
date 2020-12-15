b01a82159dd791b93101adc50e9c4db14486e1aa
f_gholizade13 <gholizadefatemeh37@gmail.com>
2020-12-15 09:18:39 -0500

    Making navbar responsive by adding burger

--- Webfront/src/Components/Navbar.js
+++ Webfront/src/Components/Navbar.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 //import {NavLink} from 'react-router-dom';
 import {Navbar,Nav,Button} from 'react-bootstrap';
@@ -90,28 +91,25 @@ useEffect(() => {
     return(   
       <nav class="navbar navbar-expand-lg navbar-light color4">
         <h1>  
-          <GiBookshelf color="white" />
+          <GiBookshelf color="black" />
         </h1>
-        <b 
-         style = {{fontSize:35,fontFamily: 'Mitra',fontWeight:"bold",color:"white"}}
-         
+        <b className="btn" 
+         style = {{fontSize:35,fontFamily: 'Mitra',fontWeight:"bold",color:"black"}}
+          onClick={routeToHome}
         >کیما</b> 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ">
             <li class="nav-item" href="groups"
-               style = {{fontFamily: 'Mitra',fontSize:25,fontWeight:"bold"}}>
-              <a class="nav-link" href="#" style={{color:"white"}}>گروه‌ها</a>
+               style = {{fontFamily: 'Mitra',fontSize:25,fontWeight:"bold",color:"black"}}>
+              <a class="nav-link" href="#">گروه‌ها</a>
             </li>
             <li class="nav-item" href="quize"
-               style = {{fontFamily: 'Mitra',fontSize:25,fontWeight:"bold",color:"white"}}>
-              <a class="nav-link" href="#" style={{color:"white"}}>آزمونک</a>
-            </li>
-            <li class="btn"
-               style = {{fontFamily: 'Mitra',fontSize:25,fontWeight:"bold",color:"white"}}>
-              <a class="nav-link" style={{color:"white"}}  onClick={routeToHome}>خانه</a>
+               style = {{fontFamily: 'Mitra',fontSize:25,fontWeight:"bold",color:"black"}}>
+              <a class="nav-link" href="#">آزمونک</a>
             </li>
           </ul>
           <div className="mr-3 d-flex ml-auto justify-content-between ">
@@ -120,10 +118,10 @@ useEffect(() => {
                style={{fontFamily:'Mitra'}}
                />             
                 <Button variant="gray" className="" onClick={handleShow}>
-                  <GoSearch size="30" color="white" />
+                  <GoSearch size="30" color="black" />
                 </Button>
                 </div>
-              <a className="row mt-1"  onClick={routeToProfile} style={{color:"white",fontFamily:'Mitra'}} >
+              <a className="row mt-1"  onClick={routeToProfile} style={{color:"black",fontFamily:'Morvarid'}} >
                <small className="" size="50" style={{padding:10,fontSize:20}}>
                 {Cookies.get('userName')}
                </small>
@@ -181,6 +179,3 @@ useEffect(() => {
     }
 
     export default withRouter( NavBar);
-
-
-
