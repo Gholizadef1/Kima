1da5bc8bd499a09b90da891a129746392ffecc39
setayeshk <setayeshk12345@gmail.com>
2020-12-17 06:01:05 +0330

    Delete unneeded code in Activityquote

--- Appfront/src/Screens/Activityquote.js
+++ Appfront/src/Screens/Activityquote.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 import { StyleSheet, Text, View ,Image,FlatList, ImageBackground, Alert,ActivityIndicator,Keyboard} from 'react-native';
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
-import Quotecrad from './Quotecard';
+import Activityquotecard from './Activityquotecard';
 import BottomSheet from 'reanimated-bottom-sheet';
 import Animated, { set } from 'react-native-reanimated';
 import {Container,Header,Title,Form,Item,Input,Button, Icon,CheckBox,Body, ActionSheet,Textarea, Content,Fab} from 'native-base';
@@ -12,18 +12,11 @@ import axiosinst from '../api/axiosinst';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AntDesign } from '@expo/vector-icons';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import { BlurView, VibrancyView } from "@react-native-community/blur";
 import { FAB } from 'react-native-paper';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 
-const commentschema=yup.object({
 
-  comment:yup.string()
-  .required("نقل قول شما نمیتواند خالی باشد")
-  .max(500,"متن نوشته شده برای اشتراک طولانی تر از حد مجاز است")
-  // .test('line',"متن نوشته شده برای اشتراک طولانی تر از حد مجاز است",(val=>(val.toString().split().length<=500)))
-})
 
 
 const Activityquote = (prop) => {
@@ -33,16 +26,8 @@ const callbackFunction = async(childData) => {
     // await setrefresh(childData)
     console.log('TRUE')
     await response(1)
-    // if(finfo===true)
-    // await setfinfo(false);
-    // else
-    // await setfinfo(true);
+   
   }
-  // console.log(refresh)
-  //  await setrefresh(childData)
-  //  console.log(yup.refresh)
-  //  if(refresh===true)
-  //  await response(1);
 }
   
   const getlike=async(item)=>{
@@ -147,109 +132,8 @@ const callbackFunction = async(childData) => {
      console.log(IDD+'IDD');
     
     },[]))
-    const renderInner=()=>{
-        return(
-          // console.log('inner');
-        <View style={{backgroundColor:'#EDF2F4'}}>
-  
-  
-  <Formik style={{}}
-                initialValues={{comment:''}}
-                validationSchema={commentschema}
-                onSubmit={async(values,actions)=>{
-              
-                  console.log('sumbit')
-                const back={
-                 textquote:values.comment,
-        
-                }
-                
-                console.log((await AsyncStorage.getItem('token')).toString())
-                const backk=JSON.stringify(back);
-                const params=JSON.stringify({username:'Hi'});
-                axiosinst.post('api/quotes/'+prop.route.params.id,backk,{"headers":
-               {
-                "Content-Type":"application/json",
-                "Authorization":"Token "+(await AsyncStorage.getItem('token')).toString()
-               }})
-              .then(async function(response){
-                  console.log(response);
-                  // await bs.current.snapTo(1)
-
-                  // await Keyboard.dismiss();
-                 
-                   setclose(true);
-                   actions.resetForm();
-                   
-                })
-              .catch(function(error){
-              console.log(error);
-         
-               })
-         
-                }}
-                 >
-              {(props)=>(
-            <View>
-
-                
-              <View style={{marginHorizontal:wp('7%'),borderColor:'red',marginTop:hp('1%')}}>
-            <Textarea rowSpan={7.5} bordered borderRadius={8} 
-                onChangeText={props.handleChange('comment')}
-                onBlur={props.handleBlur('comment')}
-                value={props.values.comment}
-                placeholder={'  نقل قول شما ...    '} placeholderTextColor='lightgray' fontSize={hp('1.8%')} style={{backgroundColor:'white'}}>
-              
-                </Textarea>
-                </View>
-        
-       
-           <Text style={{fontSize:wp('2%'), color:'red',marginLeft:('10%')}}>{props.touched.comment&&props.errors.comment}</Text>
-      
-            <View style={{flexDirection:'row',width:'100%',marginRight:wp('3%'),marginLeft:wp('3%')}}>
-       
-    
-          
-   
-     <Button bordered rounded style={{backgroundColor:'#1F7A8C',borderRadius:18,height:hp('4%'),width:wp('40%'),marginLeft:wp('28%'),marginBottom:hp('2%')}}
-       onPress={props.handleSubmit}
-       >
-         <Text style={{color:'#ffff', fontSize:15,fontWeight:'bold',marginLeft:wp('32%'),width:'100%'}}>ثبت</Text>
-        </Button>
-      
-     </View>
-     </View>    
-       
-     )}
-
-     </Formik>  
-          </View>
-            )
-          }
-        
-        
-    const bs = React.createRef()
-    const fall=new Animated.Value(1);
     const [showbutton,setshowbutton]=useState(true);
-    const renderHeader=()=>{
-        console.log('header')
-        return(
-          <View style={styles.header}>
-          <View style={styles.panelHeader}>
-            <View style={styles.panelHandle} >
-            {/* <View style={{borderRadius:100}}> */}
-            <ImageBackground borderRadius={100}
-           source={require('../../assets/line5.png')}
-           style={{width:wp('21%'),height:hp('0.3%'),alignSelf:'center',borderRadius:100}}
-           ></ImageBackground>
-           {/* </View> */}
-               <Text style={{alignSelf:'center',fontWeight:'bold',color:'#1f7a8c',marginTop:hp('1.3%'),marginBottom:hp('1.3%'),fontSize:wp('4%') }}>نقل قول شما چیست؟</Text>
-            </View>
-          </View>
-        </View>
    
-          )
-      }
     return(
 
 
@@ -270,26 +154,7 @@ const callbackFunction = async(childData) => {
             dropDownStyle={{backgroundColor: '#fafafa'}}
             onChangeItem={(item) => console.log(item.value)}
         />
-        <BottomSheet style={{position:''}}
         
-       snapPoints={[hp('40%'), 0, 0]}
-      ref={bs}
-      initialSnap={1}
-      callbackNode={fall}
-      enabledGestureInteraction={true}
-      enabledContentTapInteraction={false}
-      onCloseEnd={()=>{
-        setshowbutton(true) 
-        response(1)
-        // if(theend===true)
-       
-        // response(page-1)
-      }}
-      renderContent={renderInner}
-      renderHeader={renderHeader}            
-      backgroundColor={'#edf2f4'}
-  
-  />
 
   <Animated.View style={{
     
@@ -313,9 +178,9 @@ const callbackFunction = async(childData) => {
      data={information}
      onEndReachedThreshold={0.5}
      
-    renderItem={({item})=>(<><Quotecrad  name={item.account.username} 
+    renderItem={({item})=>(<><Activityquotecard  name={item.account.username} 
 
-    date={item.sendtime.toString().split('T')[0]} lastinfo={finfo} DELETE={callbackFunction} RESPONSE={response} page={setpage} INFO={setfinfo} IDD={IDD}quoteid={item.id} id={item.account.id} height={hp('42.5%')} picture={`http://1c53ec0001dc.ngrok.io${item.account.profile_photo}`} naghlghol={item.quote_text} ></Quotecrad>
+    date={item.sendtime.toString().split('T')[0]} lastinfo={finfo} DELETE={callbackFunction} RESPONSE={response} page={setpage} INFO={setfinfo} IDD={IDD}quoteid={item.id} id={item.account.id} height={hp('42.5%')} picture={`http://1c53ec0001dc.ngrok.io${item.account.profile_photo}`} naghlghol={item.quote_text} ></Activityquotecard>
     
 <Text style={styles.heartnumber}>{item.Likes}</Text>
 
@@ -326,17 +191,7 @@ const callbackFunction = async(childData) => {
     </FlatList>
            </Animated.View>
    
-           {showbutton?<Button style={styles.addcomment}
-    onPress={()=>{
-     setshowbutton(false) 
-      bs.current.snapTo(0)
-      }}
-    >
-
-    <Text style={styles.nazar}>نقل قول شما چیست؟</Text>
-
-    </Button>
-    :null}
+        
      </View>
     );
 }
@@ -354,31 +209,8 @@ const styles = StyleSheet.create({
         fontWeight:'bold',
         color:'#EDF2F4'
     },
-    header: {
-        backgroundColor: '#EDF2F4',
-        shadowColor: 'black',
-        shadowOffset: {width: -1, height: -3},
-        shadowRadius: 20,
-        shadowOpacity: 0.5,
-        // elevation: 5,
-        paddingTop: 20,
-        borderTopLeftRadius: 20,
-        borderTopColor:'black',
-        borderTopRightRadius: 20,
-      },
-      panelHeader: {
-        borderTopColor:'black',
+ 
     
-      },  
-       addcomment:{
-    
-        width:wp('70%'),
-        marginHorizontal:'15%',
-        marginTop:hp('80.1%'),
-        position:'absolute',
-        borderRadius:17,
-        backgroundColor:'#1F7A8C'
-    },
     heart:{
       position:'absolute',
       marginTop:hp('47.5%'),
