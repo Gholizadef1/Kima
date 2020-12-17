613afc23410eb9e06e254c4677fb209a4f801618
setayeshk <setayeshk12345@gmail.com>
2020-12-17 07:09:40 +0330

    Change remaining ngrok URLs

--- Appfront/src/Screens/SignUp.js
+++ Appfront/src/Screens/SignUp.js
@@ -87,7 +87,7 @@ const signschema=yup.object({
         //    console.log(errors)
         //  }
         const params=JSON.stringify({username:'Hi',email:'Hi@Hi.Hi',password:'12345',password2:'12345'});
-        axios.post('http://1c53ec0001dc.ngrok.io/register',backk,{"headers":{"content-type":"application/json",}})
+        axios.post('http://1244af18f7bf.ngrok.io/register',backk,{"headers":{"content-type":"application/json",}})
         .then(async function(response){
           // AsyncStorage.setItem('token',response.data.token)
           
