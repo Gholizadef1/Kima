9bd4ae03ad81dd6b86aa83bb35039613b64baab7
setayeshk <setayeshk12345@gmail.com>
2020-12-22 17:10:10 +0330

    Install react-navigation/material-top-tabs

--- Appfront/src/Screens/Groups.js
+++ Appfront/src/Screens/Groups.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Segment, Content } from 'native-base';
 
 const Groups = () => {
     return(
-        <View style={styles.container}>
-            <Text>
-                Groups
-            </Text>
-        </View>
+        <Container style={{marginTop:100}}>
+        <Header hasSegment>
+          <Left>
+            <Button transparent>
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Segment>
+              <Button first><Text>Puppies</Text></Button>
+              <Button last active><Text>Cubs</Text></Button>
+            </Segment>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name="search" />
+            </Button>
+          </Right>
+        </Header>
+        <Content padder>
+          <Text>Awesome segment</Text>
+        </Content>
+      </Container>
     );
 }
 
