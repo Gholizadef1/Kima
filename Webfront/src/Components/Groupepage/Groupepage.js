443fde1cb06f74f39e448430a4dd6f4a30295d2e
f_gholizade13 <gholizadefatemeh37@gmail.com>
2020-12-26 19:50:21 +0330

    Add group bio in groupe page

--- Webfront/src/Components/Groupepage/Groupepage.js
+++ Webfront/src/Components/Groupepage/Groupepage.js
@@ -1,7 +1,10 @@
 import React, {CuseState,useEffect} from "react";
 import axios from 'axios';
 import image from "../../assets/images.jpeg";
+import images from "../../assets/image.jpeg";
 import './Groupepage.css';
+import Divider from '@material-ui/core/Divider';
+
 import {
     BrowserRouter as Router,
     Switch,
@@ -22,8 +25,22 @@ import {
         <div class="card cardG">
   
   <div class="card-body">
+    <img src={images} className="imageg img-responsive"></img>
   </div>
     </div>
+    <div className="mt-n5">
+    <b className="title-g" style={{fontFamily:'Yekan',fontSize:25,top:-15,position:"relative",left:40}}>آشفته‌حالان بیداربخت</b>
+    </div>
+    <hr className="line-g" style={{width:"43%"}}></hr>
+    <div class="card cardb">
+  <div class="card-body">
+  <p>اسکندر و سمندر در گردباد: سمندر و اسکندر از بالکن خانه‌های هم با یکدیگر صحبت می‌کنند؛ اما یک روز سمندر حرفی میزند که به ضررش تمام می‌شود.
+
+</p>
+  </div>
+    </div>
+    
+
     </div>
   
 
