16b3323e20ae7be8131b1ecd0ce03126c9d8f0b7
fomid8 <fati.omid2000@gmail.com>
2020-12-28 18:12:34 +0330

    Show all groups infos in groups page

--- Webfront/src/components/groups/groupsPage.js
+++ Webfront/src/components/groups/groupsPage.js
@@ -20,6 +20,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Cookies from 'js-cookie';
+import { red } from "@material-ui/core/colors";
 
 
 function GroupsPage (props){
@@ -105,11 +106,13 @@ const handleCreateGroupSubmit =(e) =>{
   })); 
 
   var formdata = new FormData()
-  formdata.append('profile_photo',state.file)
+  formdata.append('photo',state.file)
 
   const payload={
         "title":newGroup.name,
-        "summary":newGroup.description
+        "summary":newGroup.description,
+        "photo":state.file
+        //formdata
   }
 
   //const back= JSON.stringify(payload)
@@ -141,7 +144,7 @@ const handleCreateGroupSubmit =(e) =>{
 const [state , setState]=useState(
   {
       navigate:false,
-      file:""
+      file:null
   }
 )
 const uploadedImage = React.useRef(null);
@@ -200,14 +203,9 @@ const handleImageUpload = e => {
 
                 style={{ display: "none",color:"white" }} />
 
-                <img src={newGroup.picture} ref={uploadedImage} alt=" انتخاب عکس" className="rounded-circle img-fluid"
+                <img src={newGroup.picture} ref={uploadedImage} alt=" انتخاب عکس" className="img-thumbnail mx-auto d-block text-center"
                 onClick={() => imageUploader.current.click()} 
-                style={{
-                  width: 120,
-                  height: 120,
-                  marginLeft:180,
-                  marginTop:-5,
-                  display: "block"}}/>
+                />
 
                   <form >
                     <TextField
@@ -220,6 +218,7 @@ const handleImageUpload = e => {
                       onChange={handleChange}
                       fullWidth
                       variant="outlined"
+
                     />
                     <TextField
                       margin="dense"
@@ -251,7 +250,7 @@ const handleImageUpload = e => {
             </div>
 
             <div className="mx-md-5 px-md-5"  >
-                <div class="row row-cols-1 row-cols-md-3 row-cols-sm-2" style={{textAlign:'right'}}>
+               
 
                 {groups.message === "No!" ? (
                  
@@ -259,26 +258,28 @@ const handleImageUpload = e => {
                  <p >گروهی برای نمایش وجود ندارد</p>
 
                 ) : (
-                  <div>
+                  <div class="row row-cols-1 row-cols-md-3 row-cols-sm-2" style={{textAlign:'right'}}>
 
                 {groups.map ((current) => (
                   
 
                   <div class="col mb-4 ">
                     <div class="card h-100 shadow-lg">
-                      <img src={`http://127.0.0.1:8000${current.x}`} class="card-img-top shadow-sm" alt={current.title}/>
+                      <img src={`http://127.0.0.1:8000${current.group_photo}`} class="card-img-top shadow-sm " alt={current.title}/>
                       <div class="card-body">
                         <h5 class="card-title">{current.title}</h5>
-                        <p class="card-text">{current.x}</p>
+                        <p class="card-text">{current.summary}</p>
                         <div className="d-flex align-items-center">
                             <svg style={{width:24,height:24}} className="mx-1" viewBox="0 0 24 24">
                                 <path fill="#00BCD4" d="M12,5.5A3.5,3.5 0 0,1 15.5,9A3.5,3.5 0 0,1 12,12.5A3.5,3.5 0 0,1 8.5,9A3.5,3.5 0 0,1 12,5.5M5,8C5.56,8 6.08,8.15 6.53,8.42C6.38,9.85 6.8,11.27 7.66,12.38C7.16,13.34 6.16,14 5,14A3,3 0 0,1 2,11A3,3 0 0,1 5,8M19,8A3,3 0 0,1 22,11A3,3 0 0,1 19,14C17.84,14 16.84,13.34 16.34,12.38C17.2,11.27 17.62,9.85 17.47,8.42C17.92,8.15 18.44,8 19,8M5.5,18.25C5.5,16.18 8.41,14.5 12,14.5C15.59,14.5 18.5,16.18 18.5,18.25V20H5.5V18.25M0,20V18.5C0,17.11 1.89,15.94 4.45,15.6C3.86,16.28 3.5,17.22 3.5,18.25V20H0M24,20H20.5V18.25C20.5,17.22 20.14,16.28 19.55,15.6C22.11,15.94 24,17.11 24,18.5V20Z" />
                             </svg>
-                            <h6 class="card-subtitle mt-1 text-muted">{current.x}عضو</h6>
+                            <h6 class="card-subtitle mt-1 text-muted">{current.members_count+1}عضو</h6>
                         </div>
                       </div>
                     </div>
                   </div>
+
+                  
 
 
                ))}
@@ -287,7 +288,6 @@ const handleImageUpload = e => {
                    )}
 
 
-                </div>
             </div>
         </div>
 
