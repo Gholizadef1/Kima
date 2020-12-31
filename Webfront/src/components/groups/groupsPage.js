d019be26072f2fe3a9b438ad1bf1131b4966264d
fomid8 <fati.omid2000@gmail.com>
2020-12-31 23:28:27 +0330

    Add show more btn for groups card to go to group page

--- Webfront/src/components/groups/groupsPage.js
+++ Webfront/src/components/groups/groupsPage.js
@@ -210,6 +210,7 @@ const handleCloseSnack = (event, reason) => {
 
   const handleGoSearchGroup = ( ) => {
     console.log( searchWord);
+    setPagesNumber(1);
     axios.get(`http://127.0.0.1:8000/api/group/search/?search=${searchWord}&search_fields=title`,{
       headers:{
      "Authorization":"Token "+Cookies.get("userToken")}
@@ -330,11 +331,19 @@ const handleCloseSnack = (event, reason) => {
                   
 
                   <div class="col mb-4">
-                    <div class="card h-100 shadow-lg" onClick={() => routeToGroupHandler(current.id)}>
-                      <img src={current.group_photo} class="card-img-top shadow-sm " alt={current.title}/>
+                    <div class="card h-100 shadow-lg" >
+                      <img src={current.group_photo} class="card-img-top shadow-sm " alt={current.title} onClick={() => routeToGroupHandler(current.id)}/>
                       <div class="card-body">
-                        <h5 class="card-title">{current.title}</h5>
-                        <p class="card-text">{current.summary}</p>
+                        <h5 class="card-title btn m-n2" onClick={() => routeToGroupHandler(current.id)} style={{fontSize:25}}>{current.title}</h5>
+                        
+                        {current.summary.length >= 80 ?(
+                          <div>
+                           <p class="card-text">{current.summary.substring(0, 80)}</p>
+                           <div className="btn text-muted"  onClick={() => routeToGroupHandler(current.id)}>بیشتر...</div>
+                           </div>
+                        ):(
+                          <p class="card-text">{current.summary}</p>
+                        )}
                         <div className="align-items-center">
                             <svg style={{width:24,height:24}} className="mx-1" viewBox="0 0 24 24">
                                 <path fill="#00BCD4" d="M12,5.5A3.5,3.5 0 0,1 15.5,9A3.5,3.5 0 0,1 12,12.5A3.5,3.5 0 0,1 8.5,9A3.5,3.5 0 0,1 12,5.5M5,8C5.56,8 6.08,8.15 6.53,8.42C6.38,9.85 6.8,11.27 7.66,12.38C7.16,13.34 6.16,14 5,14A3,3 0 0,1 2,11A3,3 0 0,1 5,8M19,8A3,3 0 0,1 22,11A3,3 0 0,1 19,14C17.84,14 16.84,13.34 16.34,12.38C17.2,11.27 17.62,9.85 17.47,8.42C17.92,8.15 18.44,8 19,8M5.5,18.25C5.5,16.18 8.41,14.5 12,14.5C15.59,14.5 18.5,16.18 18.5,18.25V20H5.5V18.25M0,20V18.5C0,17.11 1.89,15.94 4.45,15.6C3.86,16.28 3.5,17.22 3.5,18.25V20H0M24,20H20.5V18.25C20.5,17.22 20.14,16.28 19.55,15.6C22.11,15.94 24,17.11 24,18.5V20Z" />
