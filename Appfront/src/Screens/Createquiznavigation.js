7fb7b2b61f3cc90548e3ff2e8972ddf0419583d3
setayeshk <setayeshk12345@gmail.com>
2021-01-25 19:24:38 +0330

    Implement create quiz and myquiz stacknavigation

---
+++ Appfront/src/Screens/Createquiznavigation.js
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View,Image,ImageBackground } from 'react-native';
+import {Container,Header,Title,Form,Item,Input,Button} from 'native-base';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+// import HomeScreen from './HomeScreen';
+import Login from './Login';
+import SignUp from './SignUp';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator, TransitionSpecs } from '@react-navigation/stack';
+// import { createStackNavigator } from 'react-navigation-stack';
+// import TabScreen from './TabScreen';
+ import Home from './Home';
+import { NavigationContainer } from '@react-navigation/native';
+import Profile from './Profile';
+import Editprofile from './Editprofile';
+import Activityquote from './Activityquote';
+import Eachgroup from './Eachgroup';
+import GroupPage from './GroupPage';
+import Myquizes from './Myquizes';
+import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import Quizes from "./Quizes";
+import Eachquiz from "./Eachquiz";
+
+
+
+
+
+const profilenav = createStackNavigator();
+const Createquiznavigation=(prop)=>{
+  return(
+
+   <profilenav.Navigator initialRouteName={'Profile'}>
+     <profilenav.Screen name='کوییز های من' component={Myquizes} options={{headerShown:false}} ></profilenav.Screen>
+     <profilenav.Screen name="ساخت کوییز جدید"   component={Eachquiz}  options={{headerShown:true,headerStyle:{backgroundColor:'#EDF2F4' },headerTitle:'کیما',headerTintColor:"#1F7A8C",headerTitleStyle:{color:'#1F7A8C',fontWeight:'bold'}}}></profilenav.Screen>
+   
+   </profilenav.Navigator>
+ 
+  )
+
+}
+
+export default Createquiznavigation;
