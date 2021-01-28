440644e8fc56689f94b2f0080de69cea28487baf
setayeshk <setayeshk12345@gmail.com>
2021-01-28 19:39:03 +0330

    Implement text area for first question of createquiz

--- Appfront/src/Screens/Createquiz.js
+++ Appfront/src/Screens/Createquiz.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, Image, View, Modal, ImageBackground, Alert, FlatList, ActivityIndicator, TextInput } from 'react-native';
+import { StyleSheet, Text, Image, View, ScrollView, Modal, ImageBackground, Alert, FlatList, ActivityIndicator, TextInput } from 'react-native';
 import { Container, Header, Left, Body, Right, Button, Icon, Title, Item, Segment, Content, Input, Label, Textarea } from 'native-base';
 // import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 // import { useFocusEffect } from '@react-navigation/native';
@@ -23,6 +23,7 @@ import * as yup from 'yup';
 import * as permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
 import { EvilIcons } from '@expo/vector-icons';
+import { RadioButton } from 'react-native-paper';
 // import { Button } from 'react-native-paper';
 const Createquiz = () => {
 
@@ -79,6 +80,7 @@ const Createquiz = () => {
         }
 
     }
+    const [value, setValue] = React.useState('1');
     return (
         <View style={styles.container}>
 
@@ -88,7 +90,7 @@ const Createquiz = () => {
             </TouchableOpacity> */}
 
             <Formik style={{ borderStyle: 'dashed', justifyContent: 'space-around' }}
-                initialValues={{ Username: '', Discription: '' ,photo:require('../../assets/quiz2.jpg')}}
+                initialValues={{ Username: '', Discription: '', photo: require('../../assets/quiz2.jpg') }}
                 // validationSchema={userschema}
                 onSubmit={async (values, actions) => {
                     console.log('ON SUBMIT')
@@ -104,102 +106,124 @@ const Createquiz = () => {
                 }}
             >
                 {(props) => (
+
                     <View style={{ marginTop: hp('5%') }}>
                         <View style={{ borderColor: 'blue' }}>
+                            <ScrollView>
 
-                            {/* // {props.values.photo === require('../../assets/quizicon.png') ?  */}
-                            {/* bedoone view nemishod in barr */}
-                            {props.values.photo === require('../../assets/quiz2.jpg') ? <TouchableOpacity style={{
-                                height: hp('14%'),
-                                marginTop: hp('0%'),
-                                width: wp('28%'),
-                                marginLeft: wp('5%'),
-                                borderRadius: 100,
-                               // position: 'absolute',
-                                borderColor: 'blue',
-                                backgroundColor:"lightgreen"
-                            }}
-                                onPress={() => { pickfromgallery(props) }}>
-                                <ImageBackground borderRadius={100}
-
-                                    source={props.values.photo}
-
-                                    style={{
-                                        height: hp('14%'),
-
-                                        marginTop: hp('0%'),
-                                        width: wp('28%'),
-                                        marginLeft: wp('0%'),
-                                        borderRadius: 20,
-                                        position: 'absolute',
-                                        //borderColor:'#1f7a8c',
-                                        //borderWidth:wp('0.2%')
-                                    }}
-                                //  onBlur={props.handleBlur('photo')}
-
-
-                                >
-
-                                </ImageBackground>
-
-                            </TouchableOpacity> : <TouchableOpacity style={styles.avatar}
-                                onPress={() => { pickfromgallery(props, props.handleChange) }}>
+                                {/* // {props.values.photo === require('../../assets/quizicon.png') ?  */}
+                                {/* bedoone view nemishod in barr */}
+                                {props.values.photo === require('../../assets/quiz2.jpg') ? <TouchableOpacity style={{
+                                    height: hp('14%'),
+                                    marginTop: hp('0%'),
+                                    width: wp('28%'),
+                                    marginLeft: wp('5%'),
+                                    borderRadius: 100,
+                                    // position: 'absolute',
+                                    borderColor: 'blue',
+                                    backgroundColor: "lightgreen"
+                                }}
+                                    onPress={() => { pickfromgallery(props) }}>
                                     <ImageBackground borderRadius={100}
-                                        source={{ uri: `${props.values.photo}` }}
-                                        onChangeItem={props.handleChange('photo')}
-                                        style={styles.avatar}
+
+                                        source={props.values.photo}
+
+                                        style={{
+                                            height: hp('14%'),
+
+                                            marginTop: hp('0%'),
+                                            width: wp('28%'),
+                                            marginLeft: wp('0%'),
+                                            borderRadius: 20,
+                                            position: 'absolute',
+                                            //borderColor:'#1f7a8c',
+                                            //borderWidth:wp('0.2%')
+                                        }}
                                     //  onBlur={props.handleBlur('photo')}
 
 
                                     >
 
                                     </ImageBackground>
-                                </TouchableOpacity>}
 
-                            <Text style={{ fontSize: hp('1.5%'), fontWeight: 'bold', color: '#1f7a8c', marginBottom: hp('1%'), marginLeft: wp('38%'),marginTop:hp("-11.3%") }}>نام کوییز</Text>
-                            <Item style={styles.item} rounded >
-                                {/* <Label style={{fontWeight:'bold'}}>نام گروه</Label> */}
-
-                                <Input style={styles.Input} autoCapitalize='words' autoCorrect={true}
-                                    onChangeText={props.handleChange('Username')}
-                                    onBlur={props.handleBlur('Username')}
-                                    value={props.values.Username}
-                                    placeholder={'نام کوییز ...'} placeholderTextColor='gray' >
-                                </Input>
-
-                                <Text style={{ fontSize: hp('1.2%'), marginLeft: wp('-3.5%'), marginTop: hp('7%'), color: 'red' }}>{props.touched.Username && props.errors.Username}</Text>
-                            </Item>
-                            <Image source={require("../../assets/quizicon.png")} style={{ height: hp("3.5%"), width: wp("7%"), position: 'absolute', marginTop: hp("6.6%"), marginLeft: wp("38%") }}></Image>
+                                </TouchableOpacity> : <TouchableOpacity style={styles.avatar}
+                                    onPress={() => { pickfromgallery(props, props.handleChange) }}>
+                                        <ImageBackground borderRadius={100}
+                                            source={{ uri: `${props.values.photo}` }}
+                                            onChangeItem={props.handleChange('photo')}
+                                            style={styles.avatar}
+                                        //  onBlur={props.handleBlur('photo')}
 
 
-                            <TouchableOpacity onPress={() => { pickfromgallery(props, props.handleChange) }} style={{ backgroundColor: '#EDF2F4', elevation: 1, height: hp('4.2%'), width: wp('8.5%'), top: hp('8%'), left: wp('-3%'), position: 'absolute', borderRadius: 100 }}>
-                                <EvilIcons onPress={() => { pickfromgallery(props, props.handleChange) }} name="camera" size={25} style={{ alignSelf: 'center', top: hp('1%') }} color="#1f7a8c" />
-                            </TouchableOpacity>
-                            <View>
-                                <Text style={{ fontSize: hp('1.5%'), fontWeight: 'bold', color: '#1f7a8c', marginBottom: hp('-5%'), marginTop: hp('8%'), marginLeft: wp('5%') }}>توضیحات</Text>
+                                        >
+
+                                        </ImageBackground>
+                                    </TouchableOpacity>}
+
+                                <Text style={{ fontSize: hp('1.5%'), fontWeight: 'bold', color: '#1f7a8c', marginBottom: hp('1%'), marginLeft: wp('38%'), marginTop: hp("-11.3%") }}>نام کوییز</Text>
+                                <Item style={styles.item} rounded >
+                                    {/* <Label style={{fontWeight:'bold'}}>نام گروه</Label> */}
+
+                                    <Input style={styles.Input} autoCapitalize='words' autoCorrect={true}
+                                        onChangeText={props.handleChange('Username')}
+                                        onBlur={props.handleBlur('Username')}
+                                        value={props.values.Username}
+                                        placeholder={'نام کوییز ...'} placeholderTextColor='gray' >
+                                    </Input>
+
+                                    <Text style={{ fontSize: hp('1.2%'), marginLeft: wp('-3.5%'), marginTop: hp('7%'), color: 'red' }}>{props.touched.Username && props.errors.Username}</Text>
+                                </Item>
+                                <Image source={require("../../assets/quizicon.png")} style={{ height: hp("3.5%"), width: wp("7%"), position: 'absolute', marginTop: hp("6.6%"), marginLeft: wp("38%") }}></Image>
+
+
+                                <TouchableOpacity onPress={() => { pickfromgallery(props, props.handleChange) }} style={{ backgroundColor: '#EDF2F4', elevation: 1, height: hp('4.2%'), width: wp('8.5%'), top: hp('8%'), left: wp('-3%'), position: 'absolute', borderRadius: 100 }}>
+                                    <EvilIcons onPress={() => { pickfromgallery(props, props.handleChange) }} name="camera" size={25} style={{ alignSelf: 'center', top: hp('1%') }} color="#1f7a8c" />
+                                </TouchableOpacity>
+                                <View>
+                                    <Text style={{ fontSize: hp('1.5%'), fontWeight: 'bold', color: '#1f7a8c', marginBottom: hp('-5%'), marginTop: hp('8%'), marginLeft: wp('5%') }}>توضیحات</Text>
+                                    <TouchableOpacity>
+                                        <Textarea rowSpan={hp('0.9.1%')} bordered borderRadius={8}
+                                            borderColor={'#EDF2F4'}
+                                            onChangeText={props.handleChange('Discription')}
+                                            onBlur={props.handleBlur('Discription')}
+                                            value={props.values.Discription}
+                                            placeholder={'توضیحات کوییز ...'} placeholderTextColor='gray' fontSize={hp('1.6.5%')} style={styles.item2}>
+
+                                        </Textarea>
+                                    </TouchableOpacity>
+                                </View>
+                                <Text style={{ fontSize: hp('1.5%'), fontWeight: 'bold', color: '#1f7a8c', marginBottom: hp('1%'), marginLeft: wp('5%'), marginTop: hp("3%") }}>سوالات:</Text>
                                 <TouchableOpacity>
-                                    <Textarea rowSpan={hp('0.9.1%')} bordered borderRadius={8}
-                                        borderColor={'lightgray'}
+                                    <Textarea rowSpan={hp('0.9.1%')} bordered borderRadius={20}
+                                        borderColor={'lightblue'}
+                                        elevation={0}
                                         onChangeText={props.handleChange('Discription')}
                                         onBlur={props.handleBlur('Discription')}
                                         value={props.values.Discription}
-                                        placeholder={'توضیحات کوییز ...'} placeholderTextColor='gray' fontSize={hp('1.6.5%')} style={styles.item2}>
+                                        placeholder={'سوال اول ...'} placeholderTextColor='gray' fontSize={hp('1.6.5%')} style={{
+                                            marginTop: hp("0%"), marginHorizontal: wp("5%"), height: hp("8%"),backgroundColor:"white"
+                                        }}>
 
                                     </Textarea>
-                                </TouchableOpacity>
-                            </View>
 
+                                </TouchableOpacity>
+                               
+                                <Text>ladkjfhlksajhflkj</Text>
+                                <Text>ladkjfhlksajhflkj</Text>
+                                <Text>ladkjfhlksajhflkj</Text>
+                                <Text>ladkjfhlksajhflkj</Text>
+                            </ScrollView>
                         </View>
-                        </View>
+                    </View>
                 )}
-                
+
 
             </Formik>
         </View>
 
         // <View style={styles.container}>
-                //   <View style={{ marginLeft: wp('2%') }}>
-                    //     <Text style={{ position: 'absolute', marginTop: 300 }}>a;ldjf;slkfjd;lksjf</Text>
+        //   <View style={{ marginLeft: wp('2%') }}>
+        //     <Text style={{ position: 'absolute', marginTop: 300 }}>a;ldjf;slkfjd;lksjf</Text>
         //     <Text style={{ position: 'absolute', marginTop: 350 }}>a;ldjf;slkfjd;lksjf</Text>
         //     <Text style={{ position: 'absolute', marginTop: 450 }}>a;ldjf;slkfjd;lksjf</Text>
         //    <View style={{ height: hp('2%') }}></View>
@@ -209,20 +233,20 @@ const Createquiz = () => {
 }
 
 const styles = StyleSheet.create({
-                container: {
-                //   position:'absolute',
-                //   marginTop:-100,
-                //   backgroundColor:'white',
-                //   height:1000,
-                flex: 1,
+    container: {
+        //   position:'absolute',
+        //   marginTop:-100,
+        //   backgroundColor:'white',
+        //   height:1000,
+        flex: 1,
         backgroundColor: '#fff',
     },
     centeredView: {
-                height: hp('40%'),
+        height: hp('40%'),
         marginTop: hp('15%'),
     },
     modalView: {
-                margin: 10,
+        margin: 10,
         backgroundColor: "white",
         borderRadius: 10,
         padding: 35,
@@ -231,30 +255,30 @@ const styles = StyleSheet.create({
     },
     plus: {
 
-                alignSelf: 'center',
+        alignSelf: 'center',
         justifyContent: 'center'
 
     },
     loader: {
-                alignItems: 'center',
+        alignItems: 'center',
         marginBottom: hp('15%'),
         justifyContent: 'center',
         alignSelf: 'center',
         marginTop: hp('10%')
     }, item2: {
-                // marginLeft:wp('-2%'),
-                // marginRight:wp('-1%'),
-                marginHorizontal: wp("5%"),
+        // marginLeft:wp('-2%'),
+        // marginRight:wp('-1%'),
+        marginHorizontal: wp("5%"),
         marginTop: hp('6%'),
         fontSize: hp('2.5%'),
     },
     item: {
-                marginLeft: wp('37%'),
+        marginLeft: wp('37%'),
         marginRight: wp('9%'),
         height: wp('9.5%')
     },
     Input: {
-         left: wp('8%'),
+        left: wp('8%'),
         fontSize: hp('1.5%'),
         fontWeight: 'bold',
         marginRight: wp('10%'),
@@ -263,7 +287,7 @@ const styles = StyleSheet.create({
         width: wp('28.5%')
     },
     avatar: {
-                height: hp('14%'),
+        height: hp('14%'),
         marginTop: hp('-1.5%'),
         width: wp('28%'),
         marginLeft: wp('-1%'),
