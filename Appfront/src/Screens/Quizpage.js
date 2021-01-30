8fe76172cada705b9da2bd188e97851e34553ebe
setayeshk <setayeshk12345@gmail.com>
2021-01-31 00:51:57 +0330

    Show hypothetical response information in quizpage

--- Appfront/src/Screens/Quizpage.js
+++ Appfront/src/Screens/Quizpage.js
@@ -37,17 +37,18 @@ const Quizpage = () => {
          quiz_photo:"/media/default.png",
          create_time:"2021-01-29"
    },
-   Questions:[{
-       question_num:1,
-       question_text:"what?",
-       a_text:"first",
-       b_text:"second",
-       c_text:"third",
-       d_text:"fourth",
+   Questions:[
+       {
+       question_num:2,
+       question_text:"در کتاب فلان صفحه ی فلان بی تو مهتاب شبی باز از آن کوچه گذشتم؟",
+       a_text:"نگذشتم",
+       b_text:"آری بی تو مهتاب شبی باز از آن کوچه گذشتم",
+       c_text:"خانه ی دوست کجاست در فلق بود",
+       d_text:"که پرسیدش سوار خونش کجاست",
        dey:"a"
    },
    {
-   question_num:1,
+   question_num:2,
    question_text:"سوال دو اینه که ...?",
    a_text:"جواب اول سوال دو",
    b_text:"جواب دوم سوال د و اینه که ",
@@ -56,13 +57,15 @@ const Quizpage = () => {
    dey:"a"}
    ]
 }
-
+const [numofquesiton,setnumofquestion]=useState(0);
+const[answers,setanswers]=useState([]);
+const [thisquesiton,setthisquestion]=useState(questions.Questions[0])
 
     return (
         <View style={styles.container}>
             <ImageBackground style={{ flex: 1 }} source={require("../../assets/fantezi4.jpg")}>
                 <ScrollView>
-                    <ProgressBar style={{ top: hp("7%"), marginHorizontal: wp("0%"), borderRadius: 20, height: hp("1%"), elevation: 3 }} progress={11 / 12} color={"#1f7a8c"} />
+                    <ProgressBar style={{ top: hp("7%"), marginHorizontal: wp("0%"), borderRadius: 20, height: hp("1%"), elevation: 3 }} progress={(numofquesiton+1) / questions.Questions[0].question_num} color={"#1f7a8c"} />
 
                     <Text style={{
                         color: "black", fontSize: hp("2%"), fontWeight: "bold", alignSelf: "center", width: wp("86%"), marginTop: hp("15%"), elevation: 5,
@@ -70,41 +73,53 @@ const Quizpage = () => {
                         // textShadowRadius: 10,
                         // textShadowColor: 'gray',
                         marginRight:wp("1%")
-                    }}>صورت سوال اول  صورت سوال اول  که؟</Text>
+                    }}>{thisquesiton.question_text}</Text>
 
 
                     <View style={{
                         backgroundColor: 'rgba(237,242,244,0.7)', height: hp("8%"), width: wp("86%"), marginTop: hp("8%"),
                         borderRadius: 100, alignSelf: "center", justifyContent: "center"
                     }}>
-                        <Text style={{ fontSize: hp("1.8%"), alignSelf: "flex-start", marginHorizontal: wp("5%") }}> پاسخ اول اینه که این پاسخ اول akfdlfنمتکنکتمنتمنتمکبشکنمتنکتمسنمکتشبنتمکسبنکمتسنبتمسنبتهست</Text>
+                        <Text style={{ fontSize: hp("1.8%"), alignSelf: "flex-start", marginHorizontal: wp("5%") }}>{thisquesiton.a_text}</Text>
                     </View>
                     <View style={{
                         backgroundColor: 'rgba(237,242,244,0.7)', height: hp("8%"), width: wp("86%"), marginTop: hp("2.5%"),
                         borderRadius: 100, elevation: 5, alignSelf: "center", justifyContent: "center"
                     }}>
-                        <Text style={{ fontSize: hp("1.8%"), alignSelf: "flex-start", marginHorizontal: wp("5%") }}> پاسخ اول اینه که این پاسخ اول akfdlfنمتکنکتمنتمنتمکبشکنمتنکتمسنمکتشبنتمکسبنکمتسنبتمسنبتهست</Text>
+                        <Text style={{ fontSize: hp("1.8%"), alignSelf: "flex-start", marginHorizontal: wp("5%") }}> {thisquesiton.b_text}</Text>
                     </View>
                     <View style={{
                         backgroundColor: 'rgba(237,242,244,0.7)', height: hp("8%"), width: wp("86%"), marginTop: hp("2.5%"),
                         borderRadius: 100, alignSelf: "center", elevation: 5, justifyContent: "center"
                     }}>
-                        <Text style={{ fontSize: hp("1.8%"), alignSelf: "flex-start", marginHorizontal: wp("5%") }}> پاسخ اول اینه که این پاسخ اول akfdlfنمتکنکتمنتمنتمکبشکنمتنکتمسنمکتشبنتمکسبنکمتسنبتمسنبتهست</Text>
+                        <Text style={{ fontSize: hp("1.8%"), alignSelf: "flex-start", marginHorizontal: wp("5%") }}> {thisquesiton.c_text}</Text>
                     </View>
                     <View style={{
                         backgroundColor: 'rgba(237,242,244,0.7)', height: hp("8%"), width: wp("86%"), marginTop: hp("2.5%"),
                         borderRadius: 100, alignSelf: "center", elevation: 5, justifyContent: "center"
                     }}>
-                        <Text style={{ fontSize: hp("1.8%"), alignSelf: "flex-start", marginHorizontal: wp("5%") }}> پاسخ اول اینه که این پاسخ اول akfdlfنمتکنکتمنتمنتمکبشکنمتنکتمسنمکتشبنتمکسبنکمتسنبتمسنبتهست</Text>
+                        <Text style={{ fontSize: hp("1.8%"), alignSelf: "flex-start", marginHorizontal: wp("5%") }}> {thisquesiton.d_text}</Text>
                     </View>
 
                     {/* <View style={{height:hp("10%"),width:wp("20%"),backgroundColor:"#1f7a8c"}}> */}
                     <View style={{ flexDirection: "row" }}>
-                        <TouchableOpacity style={{ height: hp("10%"), elevation: 5, width: wp("25%"), backgroundColor: "#1f7a8c", marginTop: hp("10.6.5%"), borderTopRightRadius: 50 }}>
+                        <TouchableOpacity
+                        onPress={async()=>{
+                            await setnumofquestion(numofquesiton-1)
+                            setthisquestion(questions.Questions[numofquesiton]);
+                            console.log("next pressed")
+                            }}
+                         style={{ height: hp("10%"), elevation: 5, width: wp("25%"), backgroundColor: "#1f7a8c", marginTop: hp("10.6.5%"), borderTopRightRadius: 50 }}>
                             <AntDesign style={{ marginTop: hp("2.2%"), marginRight: wp("11%"), color: "#Edf2f4" }} name="arrowright" size={24} color="black" />
                             <Text style={{ marginLeft: wp("4.4%"), marginTop: hp("0.4%"), fontSize: hp("1.5.5%"), fontWeight: "bold", color: "#Edf2f4" }}>سوال قبلی</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={{ height: hp("10%"), elevation: 5, width: wp("25%"), backgroundColor: "#1f7a8c", marginLeft: wp("50%"), marginTop: hp("10.6.5%"), borderTopLeftRadius: 50, alignSelf: "flex-end" }}>
+                        <TouchableOpacity
+                        onPress={async()=>{
+                            await setnumofquestion(numofquesiton+1)
+                            setthisquestion(questions.Questions[numofquesiton]);
+                            console.log("next pressed")
+                        }}
+                         style={{ height: hp("10%"), elevation: 5, width: wp("25%"), backgroundColor: "#1f7a8c", marginLeft: wp("50%"), marginTop: hp("10.6.5%"), borderTopLeftRadius: 50, alignSelf: "flex-end" }}>
                             <AntDesign style={{ marginTop: hp("2.2%"), marginRight: wp("9%"), color: "#Edf2f4" }} name="arrowleft" size={24} color="black" />
                             <Text style={{ marginLeft: wp("6.4%"), marginTop: hp("0.4%"), fontSize: hp("1.5.5%"), fontWeight: "bold", color: "#Edf2f4" }}>سوال بعدی</Text>
                         </TouchableOpacity>
