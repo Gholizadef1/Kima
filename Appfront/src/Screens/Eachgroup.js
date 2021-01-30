43685111e0c345a8da0cda4844d07b2bc7aa51f8
Mohadeseh <modi.moti78@gmail.com>
2021-01-30 16:54:57 +0330

    Change each page url

--- Appfront/src/Screens/Eachgroup.js
+++ Appfront/src/Screens/Eachgroup.js
@@ -12,13 +12,17 @@ import { AntDesign } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons'; 
 import { TouchableOpacity } from 'react-native';
 import { color } from 'react-native-reanimated';
+import Groups from './Groups';
 
 
 const Eachgroup = (prop) => {
 
   const [more, setmore] = useState(false);
+  console.log(prop.groupphoto);
+  console.log(prop.isownerid+";kasfj;lsf;lkjsf;lakj;slk;lskfj;lkj")
   const [showmore, setshowmore] = useState('بیشتر...');
   const commentt = `${prop.discription}`.toString();
+  // const [moreclicked,setmoreclicked]=useState(false);
   const linenumber = (commentt.split('').length)
   // console.log(linenumber+'  LINE NUMBER')
   const commenttt = `${prop.discription}`.toString().split('');
@@ -39,14 +43,29 @@ const Eachgroup = (prop) => {
   // console.log('*****************'+prop.discription+'  prop discription*********************')
  
     return(
-      
+    
+      // <TouchableOpacity onPress={async()=>{
+      //   console.log(moreclicked+' MORECLICKED')
+         
+      //   if(await(moreclicked===false)){
+      //   console.log(prop.id+'####')
+      //   // setmoreclicked(true);
+      //   Groups.navigation.navigate('ShowGroupPage',{id:prop.id})}}}>
       <View>
+{/*      
+      <TouchableOpacity
+      onPress={()=>{
+        console.log('^^^^^^^^^^^^^^'+'GROUP PRESSED')
+        //prop.gotogp(true);
+      }}
+      > */}
       {/* {prop.discription!='' ? (<View > */}
+      <View style={{flexDirection:'row'}}>
           <View style={styles.avatarname}>
      
      <TouchableOpacity style={styles.avatar}
-       onPress={() => { }}>
-       {prop.groupphoto === 'http://505a2dd8d5cc.ngrok.io/media/default.png' ? <ImageBackground borderRadius={100}
+       onPress={() => {}}>
+       {prop.groupphoto === '/media/default.png' ? <ImageBackground borderRadius={100}
 
          source={require('../../assets/group.jpg')}
          style={ { height: hp('8%'),
@@ -71,15 +90,17 @@ const Eachgroup = (prop) => {
      {/* <Text style={styles.username}><Text>#</Text>{prop.title}</Text> */}
 
      <Text style={styles.username}>{prop.title}</Text>
-
+     {/* prop.isownerid===new Promise(async(resolve,reject)=>{return(await AsyncStorage.getItem("id"))}) */}
      {prop.isowner?<Text style={styles.yourgroup}>#گروه شما</Text>:null}
      <Text style={styles.date}>{prop.membernumber}<Text style={{color:'gray'}}> عضو</Text></Text>
-
+    
 
     
    </View>
-  
+   </View>
+   {/* </TouchableOpacity> */}
    <View style={{ flexDirection: 'row' }}>
+ 
 
    <View style={styles.comment}>
 
@@ -88,24 +109,36 @@ const Eachgroup = (prop) => {
         {/* {!more ? <Text>{comment4}</Text> : <Text>{prop.comment}</Text>} */}
         {!more ? <Text style={{color:'black'}}>{comment4}</Text>:<Text style={{color:'black'}}>{prop.discription}</Text>}
         {linenumber>= 250 ? <TouchableOpacity
-      
+  
           onPress={async() => {
+            console.log('MORE ON PRESSSSS')
+            console.log(prop.moreclickedD+' PROP MORE CLICKED IN EACHGROUP')
+            //setmoreclicked(true)
+            //prop.gotogp(false);
+             await prop.moreclickedd(true);
+            console.log(prop.moreclickedD+' PROP MORE CLICKED IN EACHGROUP')
+    
             // if(likeshode===true)
             // await setlikeshode(false)
             // else
             // await setlikeshode(true)
-            if (more === false) {
-              setmore(true)
-              setshowmore('کم تر')
+            // if (more === false) {
+            //   setmore(true)
+            //   setshowmore('کم تر')
 
-            }
-            else {
-              setmore(false)
-              setshowmore('بیشتر...')
-            }
+            // }
+            // else {
+            //   setmore(false)
+            //   setshowmore('بیشتر...')
+            // }
+             //await prop.moreclickedd(false);
+             //prop.gotogp(true);
+            //setmoreclicked(false)
+            console.log(prop.moreclickedD+' PROP MORE CLICKED IN EACHGROUP')
+             
           }}
-          style={{ marginTop:hp('1%'),left:wp('75%')}}
-        ><Text style={{ color: '#1f7a8c' }}>{showmore}</Text>
+          style={{marginTop:hp('0.5%'),left:wp('70%'),backgroundColor:'white',height:hp('3%'),width:wp('40%')}}
+        ><Text style={{ color: '#1f7a8c'}}>{showmore}</Text>
         </TouchableOpacity> : null}
  </View>
  
@@ -122,7 +155,9 @@ const Eachgroup = (prop) => {
             </Text> */}
         {/* </View>):(<Text style={{ color: 'blue', alignSelf: 'center', marginTop: hp('30%'),
          fontWeight: 'bold' }}>نقل قولی وجود ندارد</Text>)} */}
+         {/* {prop.children} */}
          </View>
+        //  </TouchableOpacity>
     );
 }
 
