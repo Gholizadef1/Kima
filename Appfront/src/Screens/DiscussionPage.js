94c8b71af8ea386ec488100e5028949aad2315d4
Mohadeseh <modi.moti78@gmail.com>
2021-01-30 17:43:47 +0330

    Create discussion page

---
+++ Appfront/src/Screens/DiscussionPage.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useDebugValue } from 'react';
+import { StyleSheet, Text, View, Modal, ImageBackground, Image, FlatList } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import axiosinst from '../api/axiosinst';
+import { Avatar } from 'react-native-paper';
+
+const DiscussionPage = (prop) => {
+
+    return(
+        <View>
+            <Text>Discussion</Text>
+        </View>
+    )
+
+}
+export default DiscussionPage;
