1c96f9e9c8da5bcc5807fe781641ea7e1b831b1a
fomid8 <fati.omid2000@gmail.com>
2021-01-31 21:02:26 +0330

    Fix some navbar warning

--- Webfront/src/Components/Navbar.js
+++ Webfront/src/Components/Navbar.js
@@ -1,10 +1,9 @@
-import Reactfrom from 'react';
+import React, { useState, useEffect } from 'react';
 import {Button} from 'react-bootstrap';
 import {GiBookshelf} from 'react-icons/gi';
 //import {CgProfile} from 'react-icons/cg';
 import axios from 'axios';
 import { Modal } from "react-bootstrap";
-import{ useState, useEffect } from "react";
  import "./UsersList.css";
 import "./Navbar.css";
 import purple from '@material-ui/core/colors/purple';
@@ -26,9 +25,9 @@ import Avatar from '@material-ui/core/Avatar';
 import {API_BASE_URL} from '../constants/apiContants';
 
 function NavBar (props){
-  const [user,setUser] = useState({user:null});
+  const [user,setUser] = useState({user:""});
   const [search,setSearch] = useState([]);
-  const [error,setError] = useState("");
+  //const [error,setError] = useState("");
   const [userpic,setuserpic] = useState("");
 
  const handleChange = event => {
@@ -107,7 +106,7 @@ useEffect(() => {
 }
 
     return(   
-      <nav class="navbar navbar-expand-lg navbar-light px-5 color4 shadow sticky-top" style={{direction:"rtl"}}>
+      <nav className="navbar navbar-expand-lg navbar-light px-5 color4 shadow sticky-top" style={{direction:"rtl"}}>
         <h1 className="mx-1 mb-n1">  
           <GiBookshelf color="white" />
         </h1>
@@ -115,21 +114,21 @@ useEffect(() => {
          style = {{fontSize:33,fontWeight:"bold",color:"white"}}
           
         >کیما</b> 
-        <button class="navbar-toggler" style={{backgroundColor:"white"}}  type="button " data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon "></span>
+        <button className="navbar-toggler" style={{backgroundColor:"white"}}  type="button " data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon "></span>
         </button>
 
-        <div class="collapse navbar-collapse " id="navbarSupportedContent">
-          <ul class="navbar-nav ml-5">
-            <li  class="nav-link btn"
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul className="navbar-nav ml-5">
+            <li  className="nav-link btn"
                style = {{fontSize:20,fontWeight:"bold",color:"white"}}>
               <a onClick={routeToGroups}>گروه‌ها</a>
             </li>
-            <li  class="nav-link btn"
+            <li  className="nav-link btn"
                style = {{fontSize:20,fontWeight:"bold",color:"white"}}>
               <a >آزمونک</a>
             </li>
-            <li class="nav-link btn"
+            <li className="nav-link btn"
                style = {{fontSize:20,fontWeight:"bold",color:"white"}}>
               <a onClick={routeToHome}>خانه</a>
             </li>
--- Webfront/src/components/discusstion/discussion.js
+++ Webfront/src/components/discusstion/discussion.js
@@ -1,22 +1,8 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from 'prop-types';
-import SwipeableViews from 'react-swipeable-views';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import {AiOutlineDislike} from 'react-icons/ai';
-import {AiOutlineLike} from 'react-icons/ai';
-import {AiFillLike} from 'react-icons/ai';
-import {AiFillDislike} from 'react-icons/ai';
 import axios from 'axios';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import {withStyles } from '@material-ui/core/styles';
 import {
     // BrowserRouter as Router,
     // Switch,
@@ -24,15 +10,10 @@ import {
     // Redirect,
     // Link,
     // useRouteMatch,
-    useHistory,
-    useParams,
+    //useHistory,
+    //useParams,
     withRouter
   } from "react-router-dom";
-  import {GoSearch} from 'react-icons/go';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import Cookies from 'js-cookie';
 import Snackbar from '@material-ui/core/Snackbar';
 import {API_BASE_URL} from '../../constants/apiContants';
@@ -49,7 +30,7 @@ function Discussion(props) {
     const [creator,setCreator] = useState([]);
 
     useEffect(()=>{
-          axios.get(API_BASE_URL + '/group/'+ props.match.params.groupId +'/discussion/'+ props.match.params.discussionId +'/chat'+'?page='+commentsPage,
+          axios.get(`${API_BASE_URL}/group/${props.match.params.groupId}/discussion/${props.match.params.discussionId}/chat?page=${commentsPage}`,
           {
             headers:{
            "Authorization":"Token "+Cookies.get("userToken")}
