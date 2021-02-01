f7e5b1787a29a74c94562b820914a3faff2623b1
fomid8 <fati.omid2000@gmail.com>
2021-02-01 12:23:28 +0330

    Add question ui in take quiz page

--- Webfront/src/components/quiz/takeQuiz.js
+++ Webfront/src/components/quiz/takeQuiz.js
@@ -34,34 +34,72 @@ function TakeQuiz(props) {
                 <div className="d-flex mx-2 pt-3">
                   <div className="d-flex flex-column flex-wrap ml-auto col-8">
                     <h1 className="my-1 rounded-lg " >عنوان {quiz.title}</h1>
-                    <p className="my-1 text-break ">ییبر اذ لابلذ بلت ذی ت دبل تبل یل دبل دشبی تعارذهتب لبا لببل ربلذ لبذ د ادد لل بل للا ل ل لبد بلد لت تپ بلبا بیل سال یبل یسبیب ل یبل ال بذ یا سیبل یب لیب ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffل یبل سیبل یا بل اب ابل ا لب
+                    <p className="my-1 text-break ">ییبر اذ لابلذ بلت ذی ت دبلببل ربلذ لبذ دسال یبل یسبیب ل یبل ال بذ یا سیبل یب لیب ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffل یبل سیبل یا بل اب ابل ا لب
                     ل الاغبهنی بتغلل  {quiz.description}</p>
-                    <div className="btn btn-info ml-auto mt-2">شروع آزمونک</div>
+                    {/* <div className="btn btn-info ml-auto mt-2">شروع آزمونک</div> */}
                   </div>
                   <div className=" px-3 border-right border-dark my-auto">
                     <Avatar className="mx-auto " alt={creator.username} src={`${API_BASE_URL}${creator.profile_photo}`}  />
                     <h5 className="text-center mt-2">فاطمه {creator.username}</h5>
-                    <p>تاریخ ساخت {quiz.creeate_time}
-                    {/* {`${quiz.creeate_time.toString().split('T')[0]}`} */}
-                    </p>
+                    <p>تاریخ ساخت {quiz.creeate_time}</p>
                   </div>
-                 
                 </div> 
               </div>
               <hr className="border border-dark"></hr>
-              <div>
+              <div className="mx-2 mt-5">
 
-                  <div>
+                  <div className="">
                       <p>{questions.questions_num}</p>
-                      <h4>صورت سوال {questions.questions_text}</h4>
+                      <h4 className="my-4">صورت سوال1 {questions.questions_text}</h4>
                       <div>
-                          <p>{questions.a_text}</p>
-                          <p>{questions.b_text}</p>
-                          <p>{questions.c_text}</p>
-                          <p>{questions.d_text}</p>
+
+                        <div className=" btn-group-toggle d-flex flex-column col-8 mx-auto " data-toggle="buttons">
+                          <label className="btn btn-outline-dark rounded-pill m-1">
+                            <input type="radio" name="options" id="option1" checked/> {questions.a_text}ggggggg
+                          </label>
+                          <label className="btn btn-outline-dark rounded-pill m-1">
+                            <input type="radio" name="options" id="option2"/> {questions.b_text}ggggggg jtjyjyjtyjy tjrhhgn hgnhgnghmghmghm
+                          </label>
+                          <label className="btn btn-outline-dark rounded-pill m-1">
+                            <input type="radio" name="options" id="option3"/>{questions.c_text}ggggggg hrt hrthrhr
+                            jytjyjyjtu
+                          </label>
+                          <label className="btn btn-outline-dark rounded-pill m-1 ">
+                            <input type="radio" name="options" id="option4"/>{questions.d_text}gggggggfg dfg dfb fdb g bgf bg 
+                          </label>
+                        </div>
+
                       </div>
+                      <hr className="border rounded-circle col-6 border-info"></hr>
 
                   </div>
+
+
+                  <div className="">
+                      <p>{questions.questions_num}</p>
+                      <h4 className="my-4">صورت سوال1 {questions.questions_text}</h4>
+                      <div>
+
+                        <div className=" btn-group-toggle d-flex flex-column col-8 mx-auto " data-toggle="buttons">
+                          <label className="btn btn-outline-dark rounded-pill m-1">
+                            <input type="radio" name="options" id="option1" checked/> {questions.a_text}ggggggg
+                          </label>
+                          <label className="btn btn-outline-dark rounded-pill m-1">
+                            <input type="radio" name="options" id="option2"/> {questions.b_text}ggggggg jtjyjyjtyyjtyjyjdtyhhhgn hgnhgnghmghmghm
+                          </label>
+                          <label className="btn btn-outline-dark rounded-pill m-1">
+                            <input type="radio" name="options" id="option3"/>{questions.c_text}ggggggg hrt hrthrhrjytjyjyjtu
+                          </label>
+                          <label className="btn btn-outline-dark rounded-pill m-1 ">
+                            <input type="radio" name="options" id="option4"/>{questions.d_text}gggggggfg dfg dfb fdb g bgf bg 
+                          </label>
+                        </div>
+
+                      </div>
+                      <hr className="border rounded-circle col-6 border-info"></hr>
+
+                  </div>
+                 
               </div>
 
           </div>
