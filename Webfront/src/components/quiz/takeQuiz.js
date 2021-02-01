c339c96a3ebf5eee15fccaf9b33098e876f6fe83
fomid8 <fati.omid2000@gmail.com>
2021-02-01 07:15:52 +0330

    Show quiz information

--- Webfront/src/components/quiz/takeQuiz.js
+++ Webfront/src/components/quiz/takeQuiz.js
@@ -3,15 +3,20 @@ import axios from 'axios';
 import {API_BASE_URL} from '../../constants/apiContants';
 import { withRouter } from "react-router-dom";
 import Cookies from 'js-cookie';
+import Avatar from '@material-ui/core/Avatar';
 
 function TakeQuiz(props) {
 
     const [quiz,setQuiz] = useState([])
+    const [creator,setCreator] = useState([]);
+    const [question,setQuestion] = useState([]);
 
     useEffect(()=>{
         axios.get(API_BASE_URL + "quiz" + props.match.params.quizId)
         .then(response=>{
-            setQuiz(response.data.chats);
+            setQuiz(response.data.Quiz);
+            setCreator(response.data.Quiz.creator);
+            setQuestion(response.data.Question);
              console.log(response);
            })
            .catch(error=>{
@@ -30,6 +35,11 @@ function TakeQuiz(props) {
                   <div className="d-flex flex-column flex-wrap mx-3">
                     <h3 className="my-1 rounded-lg " >{quiz.title}</h3>
                     <p className="my-1 ">{quiz.description}</p>
+                  </div>
+                  <div>
+                    <Avatar className="" alt={creator.username} src={`${API_BASE_URL}${creator.profile_photo}`} style={{width:60, height:60}} />
+                    <h5 className="text-center mt-2">{creator.username}</h5>
+                    <p>تاریخ ساخت {quiz.creeate_time}</p>
                   </div>
                 </div> 
               </div>
