cefc2247d6c7d54719df25a57e0b757953d7848e
fomid8 <fati.omid2000@gmail.com>
2021-02-01 07:06:25 +0330

    Add get quiz information request

--- Webfront/src/components/quiz/takeQuiz.js
+++ Webfront/src/components/quiz/takeQuiz.js
@@ -1,10 +1,44 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {API_BASE_URL} from '../../constants/apiContants';
 import { withRouter } from "react-router-dom";
 import Cookies from 'js-cookie';
 
 function TakeQuiz(props) {
+
+    const [quiz,setQuiz] = useState([])
+
+    useEffect(()=>{
+        axios.get(API_BASE_URL + "quiz" + props.match.params.quizId)
+        .then(response=>{
+            setQuiz(response.data.chats);
+             console.log(response);
+           })
+           .catch(error=>{
+             console.log(error);
+           });
+    })
+
+
+  return(
+    <div>
+      <div className="mx-md-5 px-md-5">
+        <div className="rTOl text-right px-md-5 mx-md-5" >
+          <div className="mx-md-5 my-5 px-md-3 shadow rounded-lg ">
+              <div>
+                <div className="d-flex flex-wrap mx-2  rounded-lg pt-3">
+                  <div className="d-flex flex-column flex-wrap mx-3">
+                    <h3 className="my-1 rounded-lg " >{quiz.title}</h3>
+                    <p className="my-1 ">{quiz.description}</p>
+                  </div>
+                </div> 
+              </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    )
 }
 
 export default withRouter(TakeQuiz);
