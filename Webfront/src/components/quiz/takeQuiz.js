83344ca6c3917b68203964cecb94cef5468c5503
fomid8 <fati.omid2000@gmail.com>
2021-02-02 21:19:01 +0330

    Fix response value for get quiz questions

--- Webfront/src/components/quiz/takeQuiz.js
+++ Webfront/src/components/quiz/takeQuiz.js
@@ -10,10 +10,12 @@ function TakeQuiz(props) {
     const [quiz,setQuiz] = useState([])
     const [creator,setCreator] = useState([]);
     const [questions,setQuestions] = useState([]);
-    // [userAnswers,setUserAnswers] = useState([]);
+    const [userAnswers,setUserAnswers] = useState([]);
+    const [userScore,setUserScore] = useState("");
+
 
     useEffect(()=>{
-        axios.get(API_BASE_URL + "quiz" + props.match.params.quizId)
+        axios.get(API_BASE_URL + "/quiz/" + 1)
         .then(response=>{
             setQuiz(response.data.Quiz);
             setCreator(response.data.Quiz.creator);
@@ -23,7 +25,7 @@ function TakeQuiz(props) {
            .catch(error=>{
              console.log(error);
            });
-    })
+    },[props.match.params.quizId])
 
     const submitAnswer = (e) => {
       e.target.setAttribute("disabled", "");
@@ -34,30 +36,40 @@ function TakeQuiz(props) {
       //document.getElementById("a").style.color = "white";
       var i;
       for(i=1;i < questions.length+1;i++){
-        var node= document.getElementById(i).getElementsByClassName("active")[0].id;
-        document.getElementById(node).style.color = "white";
-        document.getElementById(node).style.backgroundColor = "red";
-        //console.log(node);
-        //console.log(questions.questions_num + node);
-
+        var chosen= document.getElementById(i).getElementsByClassName("active")[0].id;
+        document.getElementById(chosen).style.color = "white";
+        document.getElementById(chosen).style.backgroundColor = "red";
+        //console.log(chosen);
+        //console.log(questions.questions_num + chosen);
         document.getElementById(i+questions[i-1].key).style.backgroundColor = "green";
+        document.getElementById(i+questions[i-1].key).style.color = "white";
+         setUserAnswers( userAnswers.push(chosen[chosen.length -1]));
+        //console.log(lastChar(chosen));
+        console.log(chosen[chosen.length -1]);
+        
        }
       
-      //console.log( document.getElementById("w").getElementsByClassName("active")[0].id);
-
-      
-
       //post answer
-      // axios.get(API_BASE_URL + "quiz" + props.match.params.quizId)
-      //   .then(response=>{
-      //       setQuiz(response.data.Quiz);
-      //       setCreator(response.data.Quiz.creator);
-      //       setQuestions(response.data.Questions);
-      //        console.log(response);
-      //      })
-      //      .catch(error=>{
-      //        console.log(error);
-      //      });
+      const payload={
+        "user_answer": userAnswers
+      }
+      //const back= JSON.stringify(userAnswers);
+      console.log(userAnswers);
+      axios.post(API_BASE_URL + "/quiz/" + 1
+      ,payload
+      ,{
+        headers:{
+        "Content-Type":"application/json",
+       "Authorization":"Token "+Cookies.get("userToken")}
+        })
+        .then(response=>{
+             console.log(response);
+             //setUserScore(response.data.score+"امتیاز شما = ");
+             setUserScore(`امتیاز شما = ${response.data.score}`);
+           })
+           .catch(error=>{
+             console.log(error);
+           });
   }
 
 
@@ -69,46 +81,65 @@ function TakeQuiz(props) {
               <div >
                 <div className="d-flex mx-2 pt-3">
                   <div className="d-flex flex-column flex-wrap ml-auto col-8">
-                    <h1 className="my-1 rounded-lg " >عنوان {quiz.title}</h1>
-                    <p className="my-1 text-break ">ییبر اذ لابلذ بلت ذی ت دبلببل ربلذ لبذ دسال یبل یسبیب ل یبل ال بذ یا سیبل یب لیب ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffل یبل سیبل یا بل اب ابل ا لب
-                    ل الاغبهنی بتغلل  {quiz.description}</p>
+                    <h1 className="my-1 rounded-lg " >{quiz.title}</h1>
+                    <p className="my-1 text-break ">{quiz.description}</p>
                   </div>
                   <div className=" px-3 border-right border-dark my-auto">
+                    <p className="text-center my-1">سازنده :</p>
                     <Avatar className="mx-auto mb-2" alt={creator.username} src={`${API_BASE_URL}${creator.profile_photo}`}  />
-                    <p className="text-center my-n1">سازنده :</p>
-                    <p className="text-center my-n1">فاطمه {creator.username}</p>
-                    <p className="text-center text-muted my-n1">{quiz.creeate_time}sss</p>
+                    <p className="text-center my-n1">{creator.username}</p>
+                    {/* <small className="text-center text-muted my-n1">{`${quiz.create_time.toString().split('T')[0]}`}</small> */}
                   </div>
                 </div> 
               </div>
               <hr className="border border-dark"></hr>
               <div className="mx-2 mt-5">
+
+
+              {questions === undefined ? (
+                 
+
+                 <p >نطری برای نمایش وجود ندارد</p>
+
+                ) : (
+                  <div>
+
+                  {questions.map ((current) => (
+
+
                   <div className="">
-                      <p>{questions.questions_num}</p>
-                      <h4 className="my-4 col-8 mx-auto ">صورت سوال1 {questions.questions_text}</h4>
+                      
+                      <h4 className="my-4 col-8 mx-auto"><p className="badge border-bottom border-left border-dark rounded-pill m-2">{current.question_num}</p>{current.question_text} </h4>
                       <div>
-                        <div className=" btn-group-toggle d-flex flex-column col-8 mx-auto " id="1" data-toggle="buttons">
-                          <label className="btn btn-outline-dark m-1" id={questions.questions_num+"a"}>
-                            <input type="radio" name={questions.questions_num} id="a" /> {questions.a_text}ggggggg
+                        <div className=" btn-group-toggle d-flex flex-column col-8 mx-auto " id={current.question_num} data-toggle="buttons">
+                          <label className="btn btn-outline-dark m-1" id={current.question_num+"a"}>
+                            <input type="radio" name={current.question_num} id="a" /> {current.a_text}
                           </label>
-                          <label className="btn btn-outline-dark m-1" id={questions.questions_num+"b"}>
-                            <input type="radio" name={questions.questions_num} id="b" /> {questions.b_text}ggggggg jtjyjyjtyjy tjrhhgn hgng hgfh hdfg dfd fg df dfg fd gdf gdf gsdflksld ew dfef gf fdg f gf bf d fdv sdf sdhgnghmghmghm
+                          <label className="btn btn-outline-dark m-1" id={current.question_num+"b"}>
+                            <input type="radio" name={current.question_num} id="b" /> {current.b_text}
                           </label>
-                          <label className="btn btn-outline-dark m-1" id={questions.questions_num+"c"}>
-                            <input type="radio" name={questions.questions_num} id="c" />{questions.c_text}ggggggg hrt hrthrhr
+                          <label className="btn btn-outline-dark m-1" id={current.question_num+"c"}>
+                            <input type="radio" name={current.question_num} id="c" />{current.c_text}
                           </label>
-                          <label className="btn btn-outline-dark m-1" id={questions.questions_num+"d"}>
-                            <input type="radio" name={questions.questions_num} id="d" />{questions.d_text}gggggggfg dfg dfb fdb g bgf bg 
+                          <label className="btn btn-outline-dark m-1" id={current.question_num+"d"}>
+                            <input type="radio" name={current.question_num} id="d" />{current.d_text}
                           </label>
-                          <label className="active" id={questions.questions_num+"none"} ></label>
+                          <label className="active" id={current.question_num+"none"} ></label>
                         </div>
                       </div>
                       <hr className="border rounded-circle col-6 border-info"></hr>
                   </div>
+                  
+                  ))}
+                  </div>
+   
+                      )} 
+
               </div>
               <div className="d-flex">
                 <button className="btn mx-auto mb-2 btn-info" onClick={submitAnswer}>پایان آزمون</button>
               </div>
+              <h3 className="border-bottom border-top my-1 pt-1 border-success text-center mx-5">{userScore}</h3>
 
           </div>
         </div>
