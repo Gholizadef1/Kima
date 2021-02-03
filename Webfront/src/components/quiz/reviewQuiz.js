28acefe47c9dacbd491bab5cadf5c0c1aa17e058
fomid8 <fati.omid2000@gmail.com>
2021-02-03 15:25:32 +0330

    Start review quiz component

---
+++ Webfront/src/components/quiz/reviewQuiz.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {API_BASE_URL} from '../../constants/apiContants';
+import { withRouter } from "react-router-dom";
+import Cookies from 'js-cookie';
+import Avatar from '@material-ui/core/Avatar';
+
+function ReviewQuiz(props) {
+
+
+
+  return(
+      <div>
+          
+      </div>
+
+
+  )
+}
+
+export default withRouter(ReviewQuiz);
