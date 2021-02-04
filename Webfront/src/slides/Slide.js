71e486e5f0d40e4cce10b8afc41ec9dd12a86605
f_gholizade13 <gholizadefatemeh37@gmail.com>
2021-02-04 23:39:33 +0330

    Make pages responsive for width more than 1500px

--- Webfront/src/slides/Slide.js
+++ Webfront/src/slides/Slide.js
@@ -98,6 +98,22 @@ function Slide(props) {
           slidesToScroll: 2,
         },
       },
+      {
+        breakpoint: 1400,
+        settings: {
+
+          slidesToShow: 9,
+          slidesToScroll: 6,
+        },
+      },
+      {
+        breakpoint: 2000,
+        settings: {
+
+          slidesToShow: 9,
+          slidesToScroll: 6,
+        },
+      },
      
     ],
   };
